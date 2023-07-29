c68e0184d8f4c9d783657a05b54405992c7e4b9c
Luis Arce <luis.arce.60088@usalesiana.edu.bo>
2023-07-29 19:11:29 -0400

    added dependences and created the structure and integration of spotify api~

---
+++ src/main/java/org/fundacionjala/virtualassistant/player/spotify/client/MusicClient.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.virtualassistant.player.spotify.client;
+
+public interface MusicClient {
+    String exchangeAuthorizationCodeForAccessToken(String authorizationCode);
+    String getUserSavedAlbumsFromSpotify(String accessToken);
+    String getUserSavedTracksFromSpotify(String accessToken);
+    String getUserFollowingArtistsFromSpotify(String accessToken);
+    String getUserPlayerInformationFromSpotify(String accessToken);
+    void playSongOnDevice(String trackUri);
+}
---
+++ src/main/java/org/fundacionjala/virtualassistant/player/spotify/controller/MusicController.java
@@ -0,0 +1,58 @@
+package org.fundacionjala.virtualassistant.player.spotify.controller;
+
+import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
+import org.fundacionjala.virtualassistant.player.spotify.client.SpotifyClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class MusicController {
+
+    private final MusicService musicService;
+    private final SpotifyClient spotifyClient;
+
+    @Autowired
+    public MusicController(MusicService musicService, SpotifyClient spotifyClient) {
+        this.musicService = musicService;
+        this.spotifyClient = spotifyClient;
+    }
+
+    @GetMapping("/")
+    public RedirectView redirectToSpotifyAuthorization() {
+        return spotifyClient.redirectToSpotifyAuthorization();
+    }
+
+    @GetMapping("/callback")
+    public ResponseEntity<String> spotifyCallback(@RequestParam("code") String code) {
+        return spotifyClient.spotifyCallback(code);
+    }
+
+    @GetMapping("/albums")
+    public ResponseEntity<String> getUserSavedAlbums() {
+        return musicService.getUserSavedAlbums();
+    }
+
+    @GetMapping("/tracks")
+    public ResponseEntity<String> getUserSavedTracks() {
+        return musicService.getUserSavedTracks();
+    }
+
+    @GetMapping("/following")
+    public ResponseEntity<String> getUserFollowingArtists() {
+        return musicService.getUserFollowingArtists();
+    }
+
+    @GetMapping("/player")
+    public ResponseEntity<String> getUserPlayerInformation() {
+        return musicService.getUserPlayerInformation();
+    }
+
+    @PostMapping("/play")
+    public ResponseEntity<String> playSong(@RequestParam("trackUri") String trackUri) {
+        return musicService.playSong(trackUri);
+    }
+}
