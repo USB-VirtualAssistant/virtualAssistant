2146981071fe3854147e917386daf73a05216986
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-07-31 19:55:46 -0400

    Update sql tables and add postgres for web

--- sql/V1_06__create_requestAnswer_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS db_schema.request_answer (
-    id_request_answer SERIAL PRIMARY KEY,
-    id_chat INT,
-    id_request INT,
-    id_response INT,
-    FOREIGN KEY (id_chat) REFERENCES db_schema.chat (id_chat),
-    FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request),
-    FOREIGN KEY (id_response) REFERENCES db_schema.response (id_response)
-);
