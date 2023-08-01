80f39affd124be434731c9e8aa8db9ec64f7b4ec
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-07-31 23:09:43 -0400

    Added tests for uploadAudio method using mocks

---
+++ src/test/java/org/fundacionjala/virtualassistant/controllers/SpeechControllerTest.java
@@ -0,0 +1,56 @@
+package org.fundacionjala.virtualassistant.controllers;
+
+import org.fundacionjala.virtualassistant.service.SpeechService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class SpeechControllerTest {
+
+    private SpeechController yourController;
+    private SpeechService speechService;
+    private MultipartFile mockFile;
+
+    @BeforeEach
+    void setUp() {
+        speechService = mock(SpeechService.class);
+        yourController = new SpeechController(speechService);
+        mockFile = mock(MultipartFile.class);
+    }
+
+    @Test
+    void uploadAudio_NonEmptyFile_FileNameIsReturned() {
+        String fileName = "test_audio.mp3";
+        when(mockFile.isEmpty()).thenReturn(false);
+        when(mockFile.getOriginalFilename()).thenReturn(fileName);
+
+        ResponseEntity<String> response = yourController.uploadAudio(mockFile);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertTrue(response.getBody().contains(fileName));
+        verify(speechService).sendRecord(fileName);
+    }
+
+    @Test
+    void uploadAudio_EmptyFile_BadRequestStatusCode() {
+        when(mockFile.isEmpty()).thenReturn(true);
+        ResponseEntity<String> response = yourController.uploadAudio(mockFile);
+
+        assertEquals(400, response.getStatusCodeValue());
+        assertTrue(response.getBody().contains("The file is empty"));
+        verify(speechService, never()).sendRecord(anyString());
+    }
+
+    @Test
+    void uploadAudio_Exception_ExceptionIsReturned() {
+        when(mockFile.isEmpty()).thenReturn(false);
+        when(mockFile.getOriginalFilename()).thenReturn("test_audio.mp3");
+        doThrow(new RuntimeException("Simulated Exception")).when(speechService).sendRecord(anyString());
+
+        assertThrows(RuntimeException.class, () -> yourController.uploadAudio(mockFile));
+    }
+}
