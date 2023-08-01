91eb2c8b7653d3da35a14be42520fd33e5cb12de
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-08-01 08:36:24 -0400

    Added UriComponentBuilder on PostRequest to correct build of header and body

--- src/main/java/org/fundacionjala/virtualassistant/controller/RequestEntityController.java
+++ src/main/java/org/fundacionjala/virtualassistant/controller/RequestEntityController.java
@@ -1,21 +1,29 @@
 package org.fundacionjala.virtualassistant.controller;
 
+import java.net.URI;
+
 import org.fundacionjala.virtualassistant.models.RequestEntity;
 import org.fundacionjala.virtualassistant.repository.RequestEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@RequestMapping("/textRequest/")
+@RequestMapping("/textRequest")
 public class RequestEntityController {
   @Autowired
   RequestEntityRepository requestEntityRepository;
 
   @PostMapping
-  public ResponseEntity<RequestEntity> createTextRequest(@RequestBody RequestEntity requestEntity) {
-    return new ResponseEntity<>(requestEntity, HttpStatus.CREATED);
+  public ResponseEntity<RequestEntity> createTextRequest(@RequestBody RequestEntity requestEntity,
+      UriComponentsBuilder ucb) {
+    RequestEntity savedRequestEntity = requestEntityRepository.save(requestEntity);
+    URI locationOfNewRequestEntity = ucb
+        .path("textRequest/{id}")
+        .buildAndExpand(savedRequestEntity.getIdRequest())
+        .toUri();
+    return ResponseEntity.created(locationOfNewRequestEntity).build();
   }
 }
