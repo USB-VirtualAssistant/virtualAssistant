c395b7ba612a21b577da783e57821517acb62708
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-07-31 20:59:59 -0400

    Deleted unnecessary imports and added try/catchs to handle exceptions

--- src/main/java/org/fundacionjala/virtualassistant/controllers/SpeechController.java
+++ src/main/java/org/fundacionjala/virtualassistant/controllers/SpeechController.java
@@ -1,12 +1,7 @@
 package org.fundacionjala.virtualassistant.controllers;
 
-import java.io.IOException;
-
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 import org.fundacionjala.virtualassistant.service.SpeechService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
