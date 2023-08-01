c2c7d3bf5bc6a713632daa6fa47d26bbed8020fd
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-07-31 20:52:58 -0400

    Update Varchar limits

--- sql/V1_03__create_request_table.sql
+++ sql/V1_03__create_request_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS db_schema.request (
     id_request SERIAL PRIMARY KEY,
     id_user INT,
-    text VARCHAR(100),
+    text VARCHAR(150),
     date TIMESTAMP,
     id_audio_mongo VARCHAR,
     FOREIGN KEY (id_user) REFERENCES db_schema.user (id_user)
