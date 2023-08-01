07f436fc0c4eff7360677e14c761d16038654983
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-07-31 22:36:03 -0400

    Added test folder

---
+++ src/test/java/org/fundacionjala/virtualassistant/VirtualAssistantApplicationTest.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.virtualassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class VirtualAssistantApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ src/test/java/org/fundacionjala/virtualassistant/repository/WhisperClientTest.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.virtualassistant.repository;
+
+import org.junit.jupiter.api.Test;
+
+class WhisperClientTest {
+
+    private WhisperClient whisperClient;
+
+    @Test
+    public void convertToText_TextConverted() {
+
+    }
+}
