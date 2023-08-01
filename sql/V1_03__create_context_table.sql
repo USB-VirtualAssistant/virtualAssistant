0e9a3c2beae11d7fd21df987ee163487e961080d
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-08-01 09:53:13 -0400

    VA-018[malaga16]: Update DB schema, updated flyway migration script.

--- sql/V1_03__create_context_table.sql
+++ sql/V1_03__create_context_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS db_schema.context (
     id_context SERIAL PRIMARY KEY,
     title VARCHAR(45),
-    id_request INT,
-    FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request)
+    id_user INT,
+    FOREIGN KEY (id_user) REFERENCES db_schema.user (id_user)
 );
