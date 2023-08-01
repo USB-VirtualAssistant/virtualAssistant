cce96bdd297f1ae564f76def70bfcf3ed87c31b9
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-07-31 23:46:50 -0400

    Post textrRequest end point created

--- src/main/java/org/fundacionjala/virtualassistant/controller/RequestEntityController.java
+++ src/main/java/org/fundacionjala/virtualassistant/controller/RequestEntityController.java
@@ -1,9 +1,21 @@
 package org.fundacionjala.virtualassistant.controller;
 
+import org.fundacionjala.virtualassistant.models.RequestEntity;
 import org.fundacionjala.virtualassistant.repository.RequestEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/textRequest/")
 public class RequestEntityController {
   @Autowired
   RequestEntityRepository requestEntityRepository;
+
+  @PostMapping
+  public ResponseEntity<RequestEntity> createTextRequest(@RequestBody RequestEntity requestEntity) {
+    return new ResponseEntity<>(requestEntity, HttpStatus.CREATED);
+  }
 }
