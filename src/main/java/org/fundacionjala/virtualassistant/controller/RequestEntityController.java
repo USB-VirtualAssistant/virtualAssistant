74941baec2ae9fbfd5dba156d8984f7c9ae90710
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-07-31 23:22:37 -0400

    RequestEntityController created and dependency repository injected

---
+++ src/main/java/org/fundacionjala/virtualassistant/controller/RequestEntityController.java
@@ -0,0 +1,9 @@
+package org.fundacionjala.virtualassistant.controller;
+
+import org.fundacionjala.virtualassistant.repository.RequestEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RequestEntityController {
+  @Autowired
+  RequestEntityRepository requestEntityRepository;
+}
