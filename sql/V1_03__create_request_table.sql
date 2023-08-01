dcf7dd8f975d19bb0c0b6632f9d04bbc019208c3
santihsdev <123203457+santihsdev@users.noreply.github.com>
2023-07-31 22:34:38 -0400

    VA-011 Request Entity with its Repository (#5)
    * Created Entity TextRequest with variable idRequest, text, date, and idAudioMongo
    
    * gradle dependencies for postgress and flyway added
    
    * new Docker file added
    
    * docker compose file added for the flyway and postgress containers
    
    * flyway migrations files added
    
    * Update sql tables and add postgres for web
    
    * Request Entity created with its repository
    
    * Test for repository added
    
    ---------
    
    Co-authored-by: Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
    Co-authored-by: CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>

---
+++ sql/V1_03__create_request_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS db_schema.request (
+    id_request SERIAL PRIMARY KEY,
+    id_user INT,
+    text VARCHAR(100),
+    date TIMESTAMP,
+    id_audio_mongo VARCHAR,
+    FOREIGN KEY (id_user) REFERENCES db_schema.user (id_user)
+);
