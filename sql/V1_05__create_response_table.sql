4097e2da2c8c37ce35d9130dad4bdccfffb8b93c
CamilaM16 <111623023+CamilaM16@users.noreply.github.com>
2023-08-01 16:35:26 -0400

    VA-18 Flyway integration (#8)
    * docker compose file added for the flyway and postgress containers
    
    * flyway migrations files added
    
    * Update sql tables and add postgres for web
    
    * Update Varchar limits
    
    * VA-018[malaga16]: Update DB schema, updated flyway migration script.
    
    ---------
    
    Co-authored-by: Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>

--- sql/V1_05__create_response_table.sql
+++ sql/V1_05__create_response_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS db_schema.response (
     id_response SERIAL PRIMARY KEY,
-    id_request INT,
-    text VARCHAR(150),
-    date TIMESTAMP,
-    FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request)
+    id_request INTEGER UNIQUE REFERENCES db_schema.request (id_request),
+    text VARCHAR(500),
+    date TIMESTAMP
 );
