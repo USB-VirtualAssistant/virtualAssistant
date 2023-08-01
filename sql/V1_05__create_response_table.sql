58b636c47a1107e3cbf928f3726659ec639b804d
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-07-31 20:52:58 -0400

    Update Varchar limits

--- sql/V1_05__create_response_table.sql
+++ sql/V1_05__create_response_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS db_schema.response (
     id_response SERIAL PRIMARY KEY,
     id_request INT,
-    text VARCHAR(150),
+    text VARCHAR(500),
     date TIMESTAMP,
     FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request)
 );
