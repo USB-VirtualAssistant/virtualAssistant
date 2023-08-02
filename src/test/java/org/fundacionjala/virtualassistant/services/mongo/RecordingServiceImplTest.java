7d61e78cf93b7cfd0bc5882e16af3c6a180e70f5
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-02 19:11:22 -0400

    VA-019[santihsdev]: Test to Recording service added using mockito

---
+++ src/test/java/org/fundacionjala/virtualassistant/services/mongo/RecordingServiceImplTest.java
@@ -0,0 +1,40 @@
+package org.fundacionjala.virtualassistant.services.mongo;
+
+import org.bson.Document;
+import org.fundacionjala.virtualassistant.models.mongo.Recording;
+import org.fundacionjala.virtualassistant.repository.mongo.RecordingRepo;
+import org.fundacionjala.virtualassistant.repository.mongo.RecordingRepositoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecordingServiceImplTest {
+  private RecordingService service;
+  private RecordingRepo recordingRepo;
+
+  @BeforeEach
+  void setUp() {
+    recordingRepo = mock(RecordingRepositoryImpl.class);
+    service = new RecordingServiceImpl(recordingRepo);
+  }
+
+  @Test
+  void saveRecordingInDB() {
+    long idUser = 12L;
+    long idChat = 13L;
+    var mockFile = new MockMultipartFile("test", new byte[10]);
+
+    Recording recording = new Recording(idUser, idChat, new Document("test", "test"));
+    when(service.saveRecording(anyLong(), anyLong(), any(MultipartFile.class)))
+            .thenReturn(recording);
+
+    Recording result = service.saveRecording(idUser, idChat, mockFile);
+    verify(recordingRepo).saveRecording(idUser, idChat, mockFile);
+    assertNotNull(result);
+    assertEquals(recording, result);
+  }
+}
