39d940d75795dd74a4f006151c0467d74827717d
Lisandro.vargas <jose.vargas.60106@usalesiana.edu.bo>
2023-08-01 23:14:44 -0400

    Added a modified of the method tha save the text [JLissandro]

--- src/main/java/org/fundacionjala/virtualassistant/repository/TextSaverImpl.java
+++ src/main/java/org/fundacionjala/virtualassistant/repository/TextSaverImpl.java
@@ -1,8 +1,26 @@
 package org.fundacionjala.virtualassistant.repository;
 
-public class TextSaverImpl implements TextSaver{
-    @Override
-    public void saveText(String text) {
+import java.sql.*;
 
+public class TextSaverImpl implements TextSaver {
+
+    private static final String DB_URL = "jdbc:postgresql://dbUrl";
+    private static final String DB_USER = "db_user";
+    private static final String DB_PASSWORD = "database_password";
+
+    @Override
+    public boolean saveText(String text, int idRequest) throws SQLException {
+        Connection connection = null;
+        PreparedStatement stmt = null;
+
+        connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        String updateQuery = "UPDATE db_schema.request SET text = ? WHERE id_request = ? RETURNING *;";
+        stmt = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, text);
+        stmt.setInt(2, idRequest);
+
+        int affectedRows = stmt.executeUpdate();
+        return affectedRows > 0;
     }
+
 }
--- src/test/java/org/fundacionjala/virtualassistant/repository/TextSaverTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/repository/TextSaverTest.java
@@ -1,6 +1,28 @@
 package org.fundacionjala.virtualassistant.repository;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.sql.*;
 public class TextSaverTest {
+
+    @Test
+    public void testSaveText_SuccessfulUpdate() throws SQLException {
+        Connection mockConnection = mock(Connection.class);
+        DriverManager mockDriverManager = mock(DriverManager.class);
+        when(mockDriverManager.getConnection(anyString(), anyString(), anyString())).thenReturn(mockConnection);
+
+        TextSaverImpl textSaver = new TextSaverImpl();
+
+        PreparedStatement mockPreparedStatement = mock(PreparedStatement.class);
+        when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockPreparedStatement);
+
+        when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+
+        boolean result = textSaver.saveText("Updated text", 123);
+
+        verify(mockPreparedStatement).setString(1, "Updated text");
+        verify(mockPreparedStatement).setInt(2, 123);
+
+        assertTrue(result);
+    }
 }
