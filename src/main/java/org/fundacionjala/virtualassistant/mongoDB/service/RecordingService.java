d0ffc80c894938b9d5e6ee918d08c1fe16ccbfa3
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-02 01:27:54 -0400

    VA-019 [SerranoAlejandroSelaya]

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongoDB/service/RecordingService.java
@@ -0,0 +1,22 @@
+package org.fundacionjala.virtualassistant.mongoDB.service;
+
+import org.fundacionjala.virtualassistant.mongoDB.model.Recording;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+
+public interface RecordingService {
+
+    Recording getRecording(Long idRecording);
+
+    boolean deleteRecording(Long idRecording);
+
+    List<Recording> getAllRecordings();
+
+    List<Recording> getAllRecordingsToUser(Long idUser, Long idChat);
+
+    Recording saveRecording(Long idUser, Long idChat, MultipartFile audioFile);
+
+}
---
+++ src/main/java/org/fundacionjala/virtualassistant/mongoDB/service/RecordingServiceImpl.java
@@ -0,0 +1,41 @@
+package org.fundacionjala.virtualassistant.mongoDB.service;
+
+import org.fundacionjala.virtualassistant.mongoDB.model.Recording;
+import org.fundacionjala.virtualassistant.mongoDB.repository.RecordingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+public class RecordingServiceImpl implements RecordingService{
+
+    @Autowired
+    RecordingRepo recordingRepo;
+
+    @Override
+    public Recording getRecording(Long idRecording) {
+        return recordingRepo.getRecording(idRecording);
+    }
+
+    @Override
+    public boolean deleteRecording(Long idRecording) {
+        return recordingRepo.deleteRecording(idRecording);
+    }
+
+    @Override
+    public List<Recording> getAllRecordingsToUser(Long idUser, Long idChat) {
+        return recordingRepo.getAllRecordingsToUser(idUser,idChat);
+    }
+
+    @Override
+    public List<Recording> getAllRecordings() {
+        return recordingRepo.getAllRecordings();
+    }
+
+    @Override
+    public Recording saveRecording(Long idUser, Long idChat, MultipartFile audioFile) {
+        return recordingRepo.saveRecording(idUser,idChat,audioFile);
+    }
+}
