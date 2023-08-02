5fa22877c37342c159c6fd07053045f43184ca51
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-08-02 15:55:36 -0400

    VA-027[valeriavalenz]: Updated code accordding to the code review and deleted unnecessary imports

--- src/test/java/org/fundacionjala/virtualassistant/controllers/SpeechControllerTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/controllers/SpeechControllerTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 
@@ -14,22 +13,22 @@ public class SpeechControllerTest {
 
     private SpeechController speechController;
     private SpeechService speechService;
-    private MultipartFile mockFile;
+    private MultipartFile multipartFile;
 
     @BeforeEach
     void setUp() {
         speechService = mock(SpeechService.class);
         speechController = new SpeechController(speechService);
-        mockFile = mock(MultipartFile.class);
+        multipartFile = mock(MultipartFile.class);
     }
 
     @Test
     void uploadAudio_NonEmptyFile_OkStatus() {
         String fileName = "audio.mp3";
-        when(mockFile.isEmpty()).thenReturn(false);
-        when(mockFile.getOriginalFilename()).thenReturn(fileName);
+        when(multipartFile.isEmpty()).thenReturn(false);
+        when(multipartFile.getOriginalFilename()).thenReturn(fileName);
 
-        HttpStatus response = speechController.uploadAudio(mockFile);
+        HttpStatus response = speechController.uploadAudio(multipartFile);
 
         assertEquals(HttpStatus.OK, response);
         verify(speechService).sendRecord(fileName);
@@ -37,19 +36,10 @@ public class SpeechControllerTest {
 
     @Test
     void uploadAudio_EmptyFile_BadRequestStatus() {
-        when(mockFile.isEmpty()).thenReturn(true);
-        HttpStatus response = speechController.uploadAudio(mockFile);
+        when(multipartFile.isEmpty()).thenReturn(true);
+        HttpStatus response = speechController.uploadAudio(multipartFile);
 
         assertEquals(HttpStatus.BAD_REQUEST, response);
         verify(speechService, never()).sendRecord(anyString());
-    }
-
-    @Test
-    void uploadAudio_Exception_ExceptionIsReturned() {
-        when(mockFile.isEmpty()).thenReturn(false);
-        when(mockFile.getOriginalFilename()).thenReturn("audio.mp3");
-        doThrow(new RuntimeException("Simulated Exception")).when(speechService).sendRecord(anyString());
-
-        assertThrows(RuntimeException.class, () -> speechController.uploadAudio(mockFile));
     }
 }
