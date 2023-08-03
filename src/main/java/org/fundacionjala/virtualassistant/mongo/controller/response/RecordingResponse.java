f3f02318b2c6cb2fa27ce345c620f44414365dca
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-03 16:46:06 -0400

    VA-019[SerranoAlejandroSelaya]: create request and response

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/response/RecordingResponse.java
@@ -0,0 +1,17 @@
+package org.fundacionjala.virtualassistant.mongo.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+import java.io.File;
+
+@Builder
+@Value
+@AllArgsConstructor
+public class RecordingResponse {
+    String idRecording;
+    Long idUser;
+    Long idChat;
+    File audioFile;
+}
