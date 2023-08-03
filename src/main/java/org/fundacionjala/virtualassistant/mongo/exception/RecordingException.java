8696d54ed3de7bbc478b2761183917dbbff2015d
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-03 19:25:42 -0400

    VA-019 [santihsdev]: Exception to manage converting document to file created

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/exception/RecordingException.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.virtualassistant.mongo.exception;
+
+public class RecordingException extends Exception {
+  public RecordingException(String message) {
+    super(message);
+  }
+}
