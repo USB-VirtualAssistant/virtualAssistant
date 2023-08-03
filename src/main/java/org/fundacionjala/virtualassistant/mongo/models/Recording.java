4154fd723e60b20b02d2f197061da224235bb674
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-03 16:47:11 -0400

    VA-019[SerranoAlejandroSelaya]: implement response and request in revice implement

--- src/main/java/org/fundacionjala/virtualassistant/mongo/models/Recording.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/models/Recording.java
@@ -1,24 +1,26 @@
 package org.fundacionjala.virtualassistant.mongo.models;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.Objects;
 
+@Builder
+@AllArgsConstructor
 @Document(collection = "recordings")
 public class Recording {
-
     @Id
     private String idRecording;
     private Long idUser;
     private Long idChat;
     private org.bson.Document audioFile;
 
-    public Recording(Long idUser, Long idChat, org.bson.Document audioFile) {
+    public Recording(Long idUser, Long idChat, org.bson.Document metadata) {
         this.idUser = idUser;
         this.idChat = idChat;
-        this.audioFile = audioFile;
+        this.audioFile = metadata;
     }
 
     public String getIdRecording() {
@@ -33,10 +35,9 @@ public class Recording {
         return idChat;
     }
 
-    public org.bson.Document getAudio() {
-        return (org.bson.Document) audioFile;
+    public org.bson.Document getAudioFile() {
+        return audioFile;
     }
-
 
     @Override
     public boolean equals(Object o) {
