5e7a2ff62822be8d1d06d70e17fb3952867d10b8
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-03 16:47:11 -0400

    VA-019[SerranoAlejandroSelaya]: implement response and request in revice implement

--- src/main/java/org/fundacionjala/virtualassistant/mongo/services/RecordingService.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/services/RecordingService.java
@@ -1,6 +1,8 @@
 package org.fundacionjala.virtualassistant.mongo.services;
 
 import org.bson.Document;
+import org.fundacionjala.virtualassistant.mongo.controller.request.RecordingRequest;
+import org.fundacionjala.virtualassistant.mongo.controller.response.RecordingResponse;
 import org.fundacionjala.virtualassistant.mongo.exception.GeneratedDocumentException;
 import org.fundacionjala.virtualassistant.mongo.models.Recording;
 import org.fundacionjala.virtualassistant.mongo.repository.RecordingRepo;
@@ -11,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,28 +30,55 @@ public class RecordingService {
     }
 
 
-    public Recording getRecording(String idRecording) {
-        return recordingRepo.getRecording(idRecording);
+    public RecordingResponse getRecording(String idRecording) {
+        Recording recording = recordingRepo.getRecording(idRecording);
+        return convertRecordingToResponse(recording);
     }
 
-    public Recording getRecordingToUserChat(String idRecording, Long idUser, Long idChat) {
-        return recordingRepo.getRecordingToUser(idRecording, idUser, idChat);
+    public RecordingResponse getRecordingToUserChat(String idRecording, Long idUser, Long idChat) {
+        Recording recording = recordingRepo.getRecordingToUser(idRecording, idUser, idChat);
+        return convertRecordingToResponse(recording);
     }
 
     public boolean deleteRecording(String idRecording) {
         return recordingRepo.deleteRecording(idRecording);
     }
 
-    public List<Recording> getAllRecordingsToUser(Long idUser, Long idChat) {
-        return recordingRepo.getAllRecordingsToUser(idUser,idChat);
+    public List<RecordingResponse> getAllRecordingsToUser(Long idUser, Long idChat) {
+        List<Recording> recordings = recordingRepo.getAllRecordingsToUser(idUser,idChat);
+        return convertListRecordingsToListResponse(recordings);
     }
 
-    public List<Recording> getAllRecordings() {
-        return recordingRepo.getAllRecordings();
+    public List<RecordingResponse> getAllRecordings() {
+        List<Recording> recordings = recordingRepo.getAllRecordings();
+        return  convertListRecordingsToListResponse(recordings);
     }
 
-    public Recording saveRecording(Long idUser, Long idChat, MultipartFile audioFile) throws GeneratedDocumentException {
-        return recordingRepo.saveRecording(idUser,idChat,audioFile);
+    public RecordingRequest saveRecording(Long idUser, Long idChat, MultipartFile audioFile) throws GeneratedDocumentException {
+        Recording recording = recordingRepo.saveRecording(idUser,idChat,audioFile);
+        return convertRecordingToRequest(recording);
+    }
+
+    private RecordingRequest convertRecordingToRequest(Recording request){
+        return RecordingRequest.builder()
+                .idUser(request.getIdUser()).
+                idChat(request.getIdChat()).
+                build();
+    }
+
+    private RecordingResponse convertRecordingToResponse(Recording recording){
+        return RecordingResponse.builder()
+                .idRecording(recording.getIdRecording())
+                .idUser(recording.getIdUser())
+                .idChat(recording.getIdChat())
+                .audioFile(convertDocumentToFile(recording.getAudioFile(),"audio.wav"))
+                .build();
+    }
+
+    private List<RecordingResponse> convertListRecordingsToListResponse(List<Recording> recordings){
+        return recordings.stream()
+                .map(this::convertRecordingToResponse)
+                .collect(Collectors.toList());
     }
 
     private File convertDocumentToFile(Document document, String outputPath){
@@ -67,3 +97,4 @@ public class RecordingService {
         }
     }
 }
+
