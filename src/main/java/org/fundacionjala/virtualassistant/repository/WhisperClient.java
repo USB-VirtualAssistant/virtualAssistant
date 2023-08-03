0c9be3ce446445eefe6e57795e947f0908463e4e
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-02 22:42:47 -0400

    add webClient

--- src/main/java/org/fundacionjala/virtualassistant/repository/WhisperClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/repository/WhisperClient.java
@@ -1,18 +1,46 @@
 package org.fundacionjala.virtualassistant.repository;
 
-import java.nio.file.Path;
-import org.springframework.stereotype.Repository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@Repository
+@Data
 public class WhisperClient implements ASRClient {
+    @Value("${asr.whisper.url}")
+    private String url;
+    @Value("${asr.whisper.post-endpoint}")
+    private String postEndpoint;
 
-    public WhisperClient() {
+    private String audioFile;
 
+    public WhisperClient(String audioFile) {
+        this.audioFile = audioFile;
     }
 
     @Override
-    public String convertToText(Path audioFile)  {
-        return "";
+    public String convertToText() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(url)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        return webClient.post()
+                .uri(postEndpoint)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(getBody(audioFile)))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 
+    private MultiValueMap<String, ?> getBody(String audioFile) {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("audio_file", new FileSystemResource(audioFile));
+        return body;
+    }
 }
---
+++ src/test/java/org/fundacionjala/virtualassistant/repository/WhisperClientTest.java
@@ -0,0 +1,27 @@
+package org.fundacionjala.virtualassistant.repository;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.junit.jupiter.api.Test;
+
+class WhisperClientTest {
+
+    private WhisperClient whisperClient;
+
+    @Test
+    public void convertToText_TextConverted() throws IOException, UnsupportedAudioFileException {
+        long start = System.currentTimeMillis();
+        whisperClient = new WhisperClient();
+        String expected = " A gold ring will please most and a girl.  The long journey home took a year.  She saw a cat in the neighbor's house.  A pink shell was found on the sandy beach.  Small children came to see him.  The grass and bushes were wet with dew.  The blind man counted his old coins.  They severe storms tore down the barn.  She called his name many times.  When you hear the bell, come quickly.";
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start; 
+        System.out.println("Time elapsed: " + timeElapsed);
+        assertEquals(expected, whisperClient.convertToText("30sec.wav"));
+    }
+}
