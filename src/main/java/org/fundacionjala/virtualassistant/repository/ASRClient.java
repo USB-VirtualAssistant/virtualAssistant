8ac141b677519d335715723003db12d2329f4297
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-02 22:42:47 -0400

    add webClient

--- src/main/java/org/fundacionjala/virtualassistant/repository/ASRClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/repository/ASRClient.java
@@ -1,9 +1,7 @@
 package org.fundacionjala.virtualassistant.repository;
 
-import org.springframework.stereotype.Repository;
 import java.nio.file.Path;
 
-@Repository
 public interface ASRClient {
 
     String convertToText(Path audioFile);
