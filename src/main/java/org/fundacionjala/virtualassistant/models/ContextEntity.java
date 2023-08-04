38b8b76eb40cfdcb31b2ae22162682a037e3f0b9
Ariel  Claros <ariel.claros.60091@usalesiana.edu.bo>
2023-08-03 21:44:06 -0400

    VA-029[ArielClaros]: Fixed code based on new feedback

--- src/main/java/org/fundacionjala/virtualassistant/models/ContextEntity.java
+++ src/main/java/org/fundacionjala/virtualassistant/models/ContextEntity.java
@@ -7,21 +7,19 @@ import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.Column;
 
+import lombok.AllArgsConstructor;
+
 @Entity
 @Table(name = "context")
+@AllArgsConstructor
 public class ContextEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idContext;
+    private Long idContext;
 
     @Column(name = "title")
     private String title;
 
     @Column(name = "id_user")
-    private long idUser;
-
-    public ContextEntity(String title, long idUser) {
-        this.title = title;
-        this.idUser = idUser;
-    }
+    private Long idUser;
 }
