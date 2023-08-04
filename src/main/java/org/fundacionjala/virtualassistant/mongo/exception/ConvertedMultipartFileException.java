b5f6e6779393f9d0b1f86fe6246edd59cdc6455e
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-03 21:28:50 -0400

    VA-019[SerranoAlejandroSelaya]: create request and modify form to convert document to file

--- src/main/java/org/fundacionjala/virtualassistant/mongo/exception/ConvertedMultipartFileException.java
+++
@@ -1,7 +0,0 @@
-package org.fundacionjala.virtualassistant.mongo.exception;
-
-public class ConvertedMultipartFileException extends Exception {
-    public ConvertedMultipartFileException(String message) {
-        super(message);
-    }
-}
