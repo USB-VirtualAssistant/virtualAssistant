b92bc35e90bdd3dec2fc7f00620077f787cc1b17
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-03 20:55:09 -0400

    VA-019[SerranoAlejandroSelaya]: send the MultipartFile to response in service implement

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/exception/ConvertedMultipartFileException.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.virtualassistant.mongo.exception;
+
+public class ConvertedMultipartFileException extends Exception {
+    public ConvertedMultipartFileException(String message) {
+        super(message);
+    }
+}
