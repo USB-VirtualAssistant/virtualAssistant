b891fdc0f60037d1bf0d46fc43739dcbcb1b56a2
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-03 22:06:21 -0400

    VA-019 [santihsdev]: Either applied to manage exceptions in stream.map and test updated

--- src/main/java/org/fundacionjala/virtualassistant/mongo/services/RecordingService.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/services/RecordingService.java
@@ -8,6 +8,8 @@ import org.fundacionjala.virtualassistant.mongo.exception.ConvertedDocumentToFil
 import org.fundacionjala.virtualassistant.mongo.exception.RecordingException;
 import org.fundacionjala.virtualassistant.mongo.models.Recording;
 import org.fundacionjala.virtualassistant.mongo.repository.RecordingRepo;
+import org.fundacionjala.virtualassistant.util.either.Either;
+import org.fundacionjala.virtualassistant.util.either.ProcessorEither;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +22,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -27,6 +30,7 @@ public class RecordingService {
 
     @Autowired
     RecordingRepo recordingRepo;
+    ProcessorEither<Exception, RecordingResponse> processorEither;
 
     private static final String AUDIO_FIELD_NAME = "audio";
 
@@ -49,12 +53,12 @@ public class RecordingService {
         return recordingRepo.deleteRecording(idRecording);
     }
 
-    public List<RecordingResponse> getAllRecordingsToUser(Long idUser, Long idChat) throws RecordingException {
+    public List<RecordingResponse> getAllRecordingsToUser(Long idUser, Long idChat) {
         List<Recording> recordings = recordingRepo.getAllRecordingsToUser(idUser,idChat);
         return convertListRecordingsToListResponse(recordings);
     }
 
-    public List<RecordingResponse> getAllRecordings() throws RecordingException {
+    public List<RecordingResponse> getAllRecordings() {
         List<Recording> recordings = recordingRepo.getAllRecordings();
         return  convertListRecordingsToListResponse(recordings);
     }
@@ -73,13 +77,18 @@ public class RecordingService {
                 .build();
     }
 
-    private List<RecordingResponse> convertListRecordingsToListResponse(List<Recording> recordings) throws RecordingException {
-        List<RecordingResponse> list = new ArrayList<>();
-        for (Recording recording : recordings) {
-            RecordingResponse recordingResponse = convertRecordingToResponse(recording);
-            list.add(recordingResponse);
-        }
-        return list;
+    private List<RecordingResponse> convertListRecordingsToListResponse(List<Recording> recordings) {
+        return recordings.stream()
+                .map(processorEither.lift(recording -> {
+                    try {
+                        return Either.Right(convertRecordingToResponse(recording));
+                    } catch (RecordingException exception) {
+                        return Either.Left(exception);
+                    }
+                }))
+                .filter(Either::isRight)
+                .map(Either::getRight)
+                .collect(Collectors.toList());
     }
 
     private File convertDocumentToFile(Document document, String outputPath) throws ConvertedDocumentToFileException {
