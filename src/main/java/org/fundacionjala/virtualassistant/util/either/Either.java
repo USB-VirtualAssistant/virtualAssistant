e7aa1890442d4f58a38441b66fbbd11919e7021a
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-03 22:06:21 -0400

    VA-019 [santihsdev]: Either applied to manage exceptions in stream.map and test updated

---
+++ src/main/java/org/fundacionjala/virtualassistant/util/either/Either.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.virtualassistant.util.either;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.function.Function;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Either<L, R> implements ProcessorEither<L, R> {
+  private final L left;
+  private final R right;
+
+  public static <L, R> Either<L, R> Left(L value) {
+    return new Either<>(value, null);
+  }
+
+  public static <L, R> Either<L, R> Right(R value) {
+    return new Either<>(null, value);
+  }
+
+  public boolean isLeft() {
+    return left != null;
+  }
+
+  public boolean isRight() {
+    return right != null;
+  }
+
+  @Override
+  public <T> Function<T, Either<L, R>> lift(Function<T, Either<L, R>> function) {
+    return function;
+  }
+}
