90d001a61ae7a822e452f2cbe43e08d77867fbd7
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-09 15:29:04 -0400

    VA-026[Ronaaldo Sandoval Z]I have resolved the comments and formatted the unitTests using given-when-then

--- src/main/java/org/fundacionjala/virtualassistant/service/ASRClient.py
+++ src/main/java/org/fundacionjala/virtualassistant/service/ASRClient.py
@@ -12,26 +12,20 @@ class enumModel(str, Enum):
     large = "large"
 
 class enumException(str, Enum):
-    not_found = "not_found"
-    not_audio = "not_audio"
+    not_found = "not_found_audio_file"
 
 @app.post('/record')
 async def convert_to_text(audio_file: UploadFile = File(...)):
-    is_valid, error = validate_audio_file(audio_file)
-    with open(audio_file.filename, "wb") as file:
-        file.write(audio_file.file.read())
-    if not is_valid:
-        return {"error": error}
+    is_valid_audio, error = validate_audio_file(audio_file)
+    if not is_valid_audio:
+        return error
     return transcribe(enumModel.tiny, audio_file.filename)
 
-def validate_audio_file(file):
-    if not file:
+def validate_audio_file(audio_file):
+    if not audio_file:
         return False, enumException.not_found
-    return True, None
-
-def validate_model(model):
-    if model not in enumModel:
-        return False, enumException.not_found
+    with open(audio_file.filename, "wb") as file:
+        file.write(audio_file.file.read())
     return True, None
 
 def transcribe(model, audio_file):
