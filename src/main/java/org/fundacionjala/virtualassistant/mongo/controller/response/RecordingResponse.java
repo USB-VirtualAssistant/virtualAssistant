366762f99e6ffee295b2d6d0f9d5eeaf131a018a
J-Ivan-MC <josue.macedo.60096@usalesiana.edu.bo>
2023-08-08 20:19:04 -0400

    VA-019 [J-Ivan-MC]:Deleted import inused and new test

--- src/main/java/org/fundacionjala/virtualassistant/mongo/controller/response/RecordingResponse.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/response/RecordingResponse.java
@@ -3,7 +3,6 @@ package org.fundacionjala.virtualassistant.mongo.controller.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
-
 import java.io.File;
 
 @Builder
