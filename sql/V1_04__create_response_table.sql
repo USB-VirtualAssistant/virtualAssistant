c47afd1fb8d07fdb21fc9e591bb3b1ef0267e8c5
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-08-09 20:29:23 -0400

    [agonzaleb107]: Rebase conflict solved

--- sql/V1_04__create_response_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS db_schema.response (
-    id_response SERIAL PRIMARY KEY,
-    text VARCHAR(150),
-    date TIMESTAMP
-);
--- sql/V1_05__create_response_table.sql
+++ sql/V1_05__create_response_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS db_schema.response (
     id_response SERIAL PRIMARY KEY,
-    id_request INTEGER UNIQUE REFERENCES db_schema.request (id_request),
-    text VARCHAR(500),
-    date TIMESTAMP
+    id_request INT,
+    text VARCHAR(150),
+    date TIMESTAMP,
+    FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request)
 );
