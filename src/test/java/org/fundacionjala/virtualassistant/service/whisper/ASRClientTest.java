4d6fe469171da52536e722ae808a1b7ca77944ce
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-10 09:05:56 -0400

    VA-026[Ronaldo Sandoval Z] Resolution of Camila's comments

--- src/test/java/org/fundacionjala/virtualassistant/service/whisper/ASRClientTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/service/whisper/ASRClientTest.java
@@ -18,27 +18,29 @@ public class ASRClientTest {
 
     @MockBean
     private WebClient.Builder webClientBuilder;
-
     private WhisperClient whisperClient;
+    private static final String URL = "http://localhost:8000";
+    private static final String POST_ENDPOINT = "/record";
+
 
     @Test
     public void uploadAudio_NonEmptyFile_OkStatus() {
-        whisperClient = new WhisperClient("src/test/resources/Question.wav");
+        whisperClient = new WhisperClient();
         WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:8000")
+                .baseUrl(URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
                 .build();
-        ResponseSpec responseSpec = sendRequest(webClient, whisperClient.getAudioFile());
+        ResponseSpec responseSpec = sendRequest(webClient, "src/test/resources/Question.wav");
         assertEquals(200, responseSpec.toEntity(String.class).block().getStatusCodeValue());
     }
 
     @Test
     public void uploadAudio_EmptyFile_BadRequestStatus() {
         WebTestClient webTestClient = WebTestClient.bindToServer()
-                .baseUrl("http://localhost:8000")
+                .baseUrl(URL)
                 .build();
         webTestClient.post()
-                .uri("/record")
+                .uri(POST_ENDPOINT)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .bodyValue("nonExistentFileData") 
                 .exchange()
@@ -47,7 +49,7 @@ public class ASRClientTest {
 
     private ResponseSpec sendRequest(WebClient webClient, String audioFile) {
         return webClient.post()
-                .uri("/record")
+                .uri(POST_ENDPOINT)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .body(BodyInserters.fromMultipartData(whisperClient.getBody(audioFile)))
                 .retrieve();
