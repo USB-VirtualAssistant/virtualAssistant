647f8e859a5ac6c4ba6096f8eeb2cf344af563af
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-10 03:25:25 -0400

    VA-026[Ronaldo Sandoval Z] Enhancement of resolution: ASR client allows all types of file extensions

--- src/main/java/org/fundacionjala/virtualassistant/service/whisper/ASRClient.py
+++ src/main/java/org/fundacionjala/virtualassistant/service/whisper/ASRClient.py
@@ -13,26 +13,38 @@ class enumModel(str, Enum):
 
 class enumException(str, Enum):
     not_found = "not_found_audio_file"
+    not_valid = "not_valid_format_audio"
+
+class AudioValidator:
+    @staticmethod
+    def validate_audio_file(audio_file):
+        if audio_file is None or audio_file.filename == "":
+            return False, enumException.not_found
+        if not audio_file.content_type.startswith("audio/"):
+            return False, enumException.not_valid
+        return True, None
+
+class FileHandler:
+    @staticmethod
+    def delete_audio(audio_file):
+        try:
+            os.remove(audio_file)
+        except FileNotFoundError:
+            pass
+
+class Transcriber:
+    @staticmethod
+    def transcribe(model, audio_file):
+        model = whisper.load_model(model)
+        result = model.transcribe(audio_file)
+        FileHandler.delete_audio(audio_file)
+        return result["text"].strip()
 
 @app.post('/record')
-async def convert_to_text(audio_file: UploadFile = File(...)):
-    is_valid_audio, error = validate_audio_file(audio_file)
-    if not is_valid_audio:
+async def convert_to_text(audio_file: UploadFile = File(None)):
+    is_valid, error = AudioValidator.validate_audio_file(audio_file)
+    if not is_valid:
         return error
-    return transcribe(enumModel.tiny, audio_file.filename)
-
-def validate_audio_file(audio_file):
-    if not audio_file:
-        return False, enumException.not_found
     with open(audio_file.filename, "wb") as file:
         file.write(audio_file.file.read())
-    return True, None
-
-def transcribe(model, audio_file):
-    model = whisper.load_model(model)
-    result = model.transcribe(audio_file)
-    delete_audio(audio_file)
-    return result["text"].strip()
-
-def delete_audio(audio_file):
-    os.remove(audio_file)
+    return Transcriber.transcribe(enumModel.tiny, audio_file.filename)
