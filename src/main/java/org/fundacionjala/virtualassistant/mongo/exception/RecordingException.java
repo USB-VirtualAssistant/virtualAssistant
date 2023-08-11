572b8b8da37894dc1b73b65b64b3b7a1a90a0b44
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-11 16:05:51 -0400

    VA-019 [santihsdev]: Not null verification for audio file added, and create exception message for it

--- src/main/java/org/fundacionjala/virtualassistant/mongo/exception/RecordingException.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/exception/RecordingException.java
@@ -4,6 +4,7 @@ public class RecordingException extends Exception {
 
   public static final String MESSAGE_RECORDING_NULL = "The object Recording is null";
   public static final String MESSAGE_NOT_WAV = "The provided file is not a valid .wav file";
+  public static final String MESSAGE_NULL_AUDIO_FILE = "The audio file is null";
 
   public RecordingException(String message) {
     super(message);
