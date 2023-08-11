f7c98844a5767d9f6c3e958194f005d2e84b099a
ronaldosandoval60102 <ronaldo.sandoval.60102@usalesiana.edu.bo>
2023-08-11 14:53:42 -0400

    VA-038[Ronaldo Sandoval] Implementation of enhancement to comply with DDD format

--- src/main/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClient.java
@@ -1,7 +1,8 @@
-package org.fundacionjala.virtualassistant.repository.whisper;
+package org.fundacionjala.virtualassistant.whisper.client;
 
 import lombok.Data;
 
+import org.fundacionjala.virtualassistant.whisper.repository.ASRClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
