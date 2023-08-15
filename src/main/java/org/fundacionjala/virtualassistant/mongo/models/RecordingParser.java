dac36081ae9ca3e1837155e930e8a409d79a5827
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-15 09:39:28 -0400

    VA-019[SerranoAlejandroSelaya]: change objectNull for a optional.ofNullable and rename test
    VA-019[SerranoAlejandroSelaya]: change atomicBoolean for a map
    
    VA-019 [santihsdev]: Move parserFromRecordingToRecordingResponse, and updates test because of SOLID

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/models/RecordingParser.java
@@ -0,0 +1,46 @@
+package org.fundacionjala.virtualassistant.mongo.models;
+
+import org.fundacionjala.virtualassistant.mongo.controller.response.RecordingResponse;
+import org.fundacionjala.virtualassistant.mongo.exception.ConvertedDocumentToFileException;
+import org.fundacionjala.virtualassistant.mongo.exception.RecordingException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import static java.util.Objects.isNull;
+
+public class RecordingParser {
+
+    private static final String AUDIO_FIELD_NAME = "audio";
+    private static final String AUDIO_EXTENSION = ".wav";
+
+    public static RecordingResponse parseToRecordingResponseFrom(Recording recording)
+            throws RecordingException {
+        if (isNull(recording)) {
+            throw new RecordingException(RecordingException.MESSAGE_RECORDING_NULL);
+        }
+        return RecordingResponse.builder()
+                .idRecording(recording.getIdRecording())
+                .idUser(recording.getIdUser())
+                .idChat(recording.getIdChat())
+                .audioFile(convertDocumentToFile(recording.getAudioFile(), AUDIO_FIELD_NAME))
+                .build();
+    }
+
+    private static File convertDocumentToFile(org.bson.Document document, String nameAudio)
+            throws ConvertedDocumentToFileException {
+        try {
+            String encodedAudio = document.getString(nameAudio);
+            byte[] audioBytes = Base64.getDecoder().decode(encodedAudio);
+            File outputFile = File.createTempFile(nameAudio, AUDIO_EXTENSION);
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(audioBytes);
+            fos.close();
+            return outputFile;
+        } catch (IOException e) {
+            throw new ConvertedDocumentToFileException(e.getMessage(), e);
+        }
+    }
+}
