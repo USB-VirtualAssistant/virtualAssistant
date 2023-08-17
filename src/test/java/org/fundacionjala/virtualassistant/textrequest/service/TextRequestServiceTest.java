a370b1cdb6f85809708ce2c9e9a397ecf40484cc
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-08-17 18:46:23 -0400

    VA-033[@CamilaM16]: Remove import with wildcard

--- src/test/java/org/fundacionjala/virtualassistant/textrequest/service/TextRequestServiceTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/textrequest/service/TextRequestServiceTest.java
@@ -12,7 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
