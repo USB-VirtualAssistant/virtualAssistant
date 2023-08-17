0ebc118e12485f973c5672c38f16e6ceeed97649
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-08-16 22:48:52 -0400

    VA-20[MariaReneH]: autowired is removed and space is removed

--- src/main/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingController.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
 import java.util.Optional;
 
