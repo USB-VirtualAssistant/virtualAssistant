1a1b42fce3dac96f710c592d9eaede571b008d9c
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-17 15:28:29 -0400

    VA-032[@SebaRosalesZ] Completed WITService microservice implementation

--- src/main/java/org/fundacionjala/virtualassistant/poc_user_intentions/service/WITService.py
+++ src/main/java/org/fundacionjala/virtualassistant/poc_user_intentions/service/WITService.py
@@ -2,7 +2,6 @@ from jproperties import Properties
 from fastapi import FastAPI, HTTPException, Query
 import httpx
 import os
-from pydantic import BaseModel
 
 root_conf_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.dirname(
@@ -15,10 +14,11 @@ with open(properties_file_data, "rb") as file:
 
 class WitAiResponseHandler:
     @staticmethod
-    def create_response(input_text: str, extracted_value):
+    def create_response(input_text: str, intent, entities):
         response = {
             "input_text": input_text,
-            "extracted_value": extracted_value
+            "intent": intent,
+            "entities": entities
         }
         return response
 
@@ -28,13 +28,14 @@ class WitAiTextProcessor:
         self.target_entity = target_entity
         self.target_property = target_property
         self.http_client = httpx.AsyncClient()
+        self.response_data = {}
 
     async def process_given_text(self, input_text: str):
         try:
             response = await self.get_response(input_text)
-            data = response.json()
-            extracted_value = self.process_val_extract(data)
-            response = WitAiResponseHandler.create_response(input_text, extracted_value)
+            self.response_data = response.json()
+            intent, entities = self.process_text()
+            response = WitAiResponseHandler.create_response(input_text, intent, entities)
             return response
         except httpx.RequestError as e:
             raise HTTPException(status_code=500, detail=f"Failed processing: {str(e)}")
@@ -45,18 +46,21 @@ class WitAiTextProcessor:
         act_headers = {"Authorization": f"Bearer {self.wit_token}"}
         return await self.http_client.get(url, params=act_given_params, headers=act_headers)
 
-    def process_val_extract(self, wit_data):
-        if self.target_entity and self.target_property:
-            return wit_data["entities"].get(self.target_entity, {}).get(self.target_property, {}).get("value")
-        return None
+    def process_text(self):
+        try:
+            intent = self.response_data['intents'][0]['name'] if 'intents' in self.response_data else None
+            entities = self.response_data['entities']
+            return intent, entities
+        except Exception as e:
+            print("Error processing text:", str(e))
+            return None, None
 
     async def close(self):
         await self.http_client.aclose()
 
-
 app = FastAPI()
 config_wit_token = properties_config.get("wit.token")
-text_processor = WitAiTextProcessor(wit_token=config_wit_token)
+text_processor = WitAiTextProcessor(wit_token=config_wit_token.data)
 
 @app.get("/handle_text/")
 async def process_text_endpoint(input_data: str = Query(...)):
