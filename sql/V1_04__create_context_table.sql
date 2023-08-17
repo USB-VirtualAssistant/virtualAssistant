a8de82e5a22915f68c6d811fa734e39802ecf1ac
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-17 09:35:58 -0400

    removed duplicated sql files

--- sql/V1_04__create_context_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS db_schema.context (
-    id_context SERIAL PRIMARY KEY,
-    title VARCHAR(45),
-    id_request INT,
-    FOREIGN KEY (id_request) REFERENCES db_schema.request (id_request)
-);
