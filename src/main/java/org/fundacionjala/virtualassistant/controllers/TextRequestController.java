a6ffb8f04b52e01236068fed6b6fb88a88d064a2
ArielClaros <ariel.claros.60091@usalesiana.edu.bo>
2023-08-16 21:47:09 -0400

    Change getTextRequests method in TextRequestController

--- src/main/java/org/fundacionjala/virtualassistant/controllers/TextRequestController.java
+++ src/main/java/org/fundacionjala/virtualassistant/controllers/TextRequestController.java
@@ -1,20 +1,20 @@
 package org.fundacionjala.virtualassistant.controllers;
 
+import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.models.RequestEntity;
 import org.fundacionjala.virtualassistant.service.TextRequestService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/textRequest")
 public class TextRequestController {
-
     private TextRequestService textRequestServiceParam;
 
-    @GetMapping("/{userId}?id_context={id_context}")
-    public List<RequestEntity> getTextRequests(@PathVariable Long userID, @RequestParam Long contextID) {
-        return textRequestServiceParam.getTextRequestByUserAndContext(userID, contextID);
+    @GetMapping("/{userId}")
+    public List<RequestEntity> getTextRequests(@PathVariable Long userId, @RequestParam(name = "id_context") Long contextID) {
+        return textRequestServiceParam.getTextRequestByUserAndContext(userId, contextID);
     }
-
 }
