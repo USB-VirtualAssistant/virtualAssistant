32ade7ddd17af2c7edadeae5450c406d850d29a4
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-17 20:40:48 -0400

    VA-037[@SebaRosalesZ]:Added  givenAudioEndpoint_whenGetRequestWithUserIdAndChatId_thenResponseStatusIsOk test on  RecordingControllerTest

--- src/test/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingControllerTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingControllerTest.java
@@ -29,4 +29,14 @@ class RecordingControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    void givenAudioEndpoint_whenGetRequestWithUserIdAndChatId_thenResponseStatusIsOk() throws Exception {
+        Long userId = 123L;
+        Long chatId = 456L;
+        mockMvc.perform(get(END_POINT_PATH + "/audio/")
+                        .param("idUser", userId.toString())
+                        .param("idChat", chatId.toString()))
+                .andExpect(status().isOk());
+    }
+
 }
