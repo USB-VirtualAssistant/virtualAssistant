5ce44696a3b922071462d86a11da1c22029fd3b8
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-08-17 23:07:11 -0400

    VA-006_01_[@PabloTercerosCC]: TaskHandler OpenNPL Implementation

---
+++ src/main/java/org/fundacionjala/virtualassistant/opennlp/TaskHandler.java
@@ -0,0 +1,56 @@
+package org.fundacionjala.virtualassistant.opennlp;
+
+import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
+import org.springframework.http.ResponseEntity;
+
+public class TaskHandler {
+
+    private final MusicService MUSIC_SERVICE;
+
+    public TaskHandler(MusicService musicService) {
+        this.MUSIC_SERVICE = musicService;
+    }
+
+    public String handleTask(String text) {
+        IntentWrapper intentWrapper = new IntentWrapper(text);
+        String result;
+
+        switch (intentWrapper.getIntent()) {
+            case "Music_Request":
+                result = handleSpotify(intentWrapper.getEntities());
+                break;
+            case "Setting_reminders":
+                result = "google";
+                break;
+            default:
+                result = "chatgpt";
+                break;
+        }
+
+        return result;
+    }
+
+    private String handleSpotify(String[] entities) {
+        String entity = (entities.length > 0) ? entities[0] : "";
+        String result;
+
+        switch (entity) {
+            case "Replay_music":
+                result = "replay";
+                break;
+            case "Reproduce_music":
+                result = "reproduce";
+                break;
+            default:
+                result = caseGetAlbums();
+                break;
+        }
+        return result;
+    }
+
+    private String caseGetAlbums() {
+        ResponseEntity<String> response = MUSIC_SERVICE.getUserSavedAlbums();
+
+        return response.getBody();
+    }
+}
--- src/test/java/org/fundacionjala/virtualassistant/task/handler/TaskHandlerTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/task/handler/TaskHandlerTest.java
@@ -1,8 +1,9 @@
 package org.fundacionjala.virtualassistant.task.handler;
 
+import org.fundacionjala.virtualassistant.opennlp.TaskHandler;
 import org.fundacionjala.virtualassistant.player.spotify.client.SpotifyClient;
 import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
-import org.fundacionjala.virtualassistant.withai.TaskHandler;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,18 +12,19 @@ import static org.mockito.Mockito.when;
 
 public class TaskHandlerTest {
 
+    public final static String ACCESS_TOKEN = "Access Token";
+    public final static String ALBUM_DATA = "{\"items\": [{\"album\": {\"name\": \"Album 1\"}}, {\"album\": {\"name\": \"Album 2\"}}]}";
+
     @Test
-    public void testGetUserSavedAlbums_Success() {
+    public void givenText_whenHandlingText_thenGetAlbums() {
         SpotifyClient spotifyClient = mock(SpotifyClient.class);
-        when(spotifyClient.getAccessToken()).thenReturn("Access Token");
+        when(spotifyClient.getAccessToken()).thenReturn(ACCESS_TOKEN);
 
-        String realAlbumData = "{\"items\": [{\"album\": {\"name\": \"Album 1\"}}, {\"album\": {\"name\": \"Album 2\"}}]}";
-        when(spotifyClient.getSavedAlbums()).thenReturn(realAlbumData);
-
+        when(spotifyClient.getSavedAlbums()).thenReturn(ALBUM_DATA);
         MusicService musicService = new MusicService(spotifyClient);
 
         TaskHandler taskHandler = new TaskHandler(musicService);
-        String response = taskHandler.handleResponse();
-        assertEquals("{\"items\": [{\"album\": {\"name\": \"Album 1\"}}, {\"album\": {\"name\": \"Album 2\"}}]}", response);
+        String response = taskHandler.handleTask("Get all my albums.");
+        assertEquals(ALBUM_DATA, response);
     }
 }
