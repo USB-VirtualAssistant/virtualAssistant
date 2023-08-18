50340989691acbb5eca616ac674c25f9e51131ce
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-17 20:05:26 -0400

    user intent method created

--- src/main/java/org/fundacionjala/virtualassistant/npl/DocumentCategorizerExample.java
+++ src/main/java/org/fundacionjala/virtualassistant/npl/DocumentCategorizerExample.java
@@ -8,27 +8,22 @@ import opennlp.tools.doccat.DocumentCategorizerME;
 import opennlp.tools.tokenize.SimpleTokenizer;
 
 public class DocumentCategorizerExample {
-    public static void main(String[] args) throws Exception {
-        long startTime = System.currentTimeMillis();
-        InputStream modelIn = new FileInputStream("src/main/resources/nlp-model/en-trained-model.bin");
-        DoccatModel model = new DoccatModel(modelIn);
+    public static String getIntent(String userRequest) {
+        InputStream modelIn = null;
+        DoccatModel model = null;
+        try {
+            modelIn = new FileInputStream("src/main/resources/nlp-model/en-trained-model.bin");
+            model = new DoccatModel(modelIn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
-
-        String document = "Calculate 2 + 2";
-
         SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
-        String[] tokens = tokenizer.tokenize(document);
-
+        String[] tokens = tokenizer.tokenize(userRequest);
         double[] probabilities = categorizer.categorize(tokens);
-        String category = categorizer.getBestCategory(probabilities);
 
-        System.out.println("Category: " + category);
-        long endTime = System.currentTimeMillis();
-        long elapsedTimeMillis = endTime - startTime;
-        double elapsedTimeSeconds = elapsedTimeMillis / 1000.0;
-        System.out.println("Time Taken: " + elapsedTimeSeconds + " s");
-
+        return categorizer.getBestCategory(probabilities);
     }
 
 }
