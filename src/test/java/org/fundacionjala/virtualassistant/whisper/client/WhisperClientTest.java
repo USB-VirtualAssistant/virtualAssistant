2f2f042fdc92050916871b858fc4ca348bb72db3
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-18 17:26:47 -0400

    ISSUE [santihsdev]: Change names in WhisperClientTest

--- src/test/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClientTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClientTest.java
@@ -23,7 +23,7 @@ public class WhisperClientTest {
     }
 
     @Test
-    public void givenAudioFileQuestion_whenConvertingToText_thenTextConverted() {
+    public void shouldReturnATextFromAnAudioFileQuestion() {
         String expected = "\"How is it weather today?\"";
         String path = "src/test/resources/Question.wav";
         when(whisperClient.convertToText(anyString())).thenReturn(expected);
@@ -35,7 +35,7 @@ public class WhisperClientTest {
     }
 
     @Test
-    public void givenAudioFile10Seconds_whenConvertingToText_thenTextConverted() {
+    public void shouldReturnATextFromAnAudioFileOf10Seconds() {
         String expected = "\"Last year Tesla shares were on fire when they went over $1,000 per share.\"";
         String path = "src/test/resources/10sec.wav";
         when(whisperClient.convertToText(anyString())).thenReturn(expected);
@@ -47,7 +47,7 @@ public class WhisperClientTest {
     }
 
     @Test
-    public void givenAudioFile20Seconds_whenConvertingToText_thenTextConverted() {
+    public void shouldReturnATextFromAnAudioFileOf20Seconds() {
         String expected = "\"Hi there, this is Valentine and if you can see this text, it means you have successfully used the Whisper API from OpenAI to transcribe this audio to text. I would really appreciate it if you could give this video a thumbs up and subscribe to my channel. Your support helps me create more tutorials like this one. Thanks a lot.\"";
         String path = "src/test/resources/20sec.wav";
         when(whisperClient.convertToText(anyString())).thenReturn(expected);
@@ -59,7 +59,7 @@ public class WhisperClientTest {
     }
 
     @Test
-    public void givenAudioFile30Seconds_whenConvertingToText_thenTextConverted() {
+    public void shouldReturnATextFromAnAudioFileOf30Seconds() {
         String expected = "\"A golden ring will please most any girl. The long journey home took a year. She saw a cat in the neighbor's house. A pink shell was found on the sandy beach. Small children came to see him. The grass and bushes were wet with dew. The blind man counted his old coins. A severe storm tore down the barn. She called his name many times. When you hear the bell come quickly.\"";
         String path = "src/test/resources/30sec.wav";
         when(whisperClient.convertToText(anyString())).thenReturn(expected);
