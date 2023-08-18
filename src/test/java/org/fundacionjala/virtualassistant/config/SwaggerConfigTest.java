d6ed4c6013ce2518a659c7449f02245e40c85128
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-17 23:56:01 -0400

    VA-037[@SebaRosalesZ]:Improved unit tests, based on PR review

--- src/test/java/org/fundacionjala/virtualassistant/config/SwaggerConfigTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/config/SwaggerConfigTest.java
@@ -8,9 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,22 +22,22 @@ class SwaggerConfigTest {
         String postgresUser = System.getenv("POSTGRES_USER");
         String postgresPassword = System.getenv("POSTGRES_PASSWORD");
         String mongoUsername = System.getenv("MONGO_INITDB_ROOT_USERNAME");
-        String mongPassword = System.getenv("MONGO_INITDB_ROOT_PASSWORD");
+        String mongoPassword = System.getenv("MONGO_INITDB_ROOT_PASSWORD");
 
         System.setProperty("POSTGRES_USER", postgresUser);
         System.setProperty("POSTGRES_PASSWORD", postgresPassword);
         System.setProperty("MONGO_INITDB_ROOT_USERNAME", mongoUsername);
-        System.setProperty("MONGO_INITDB_ROOT_PASSWORD", mongPassword);
+        System.setProperty("MONGO_INITDB_ROOT_PASSWORD", mongoPassword);
     }
 
     @Test
-    void givenSwaggerUIEndpoint_whenGetRequest_thenResponseStatusIsOk() throws Exception {
+    void shouldReturnOkStatusForSwaggerUIEndpoint() throws Exception {
         mockMvc.perform(get("/swagger-ui/"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void givenSwaggerResourcesEndpoint_whenGetRequest_thenResponseStatusIsOkAndContentTypeIsJSON() throws Exception {
+    void shouldReturnOkStatusAndJSONContentTypeForSwaggerResourcesEndpoint() throws Exception {
         mockMvc.perform(get("/swagger-resources"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
@@ -47,7 +45,7 @@ class SwaggerConfigTest {
     }
 
     @Test
-    void givenSwaggerAPIDocsEndpoint_whenGetRequest_thenResponseStatusIsOk() throws Exception {
+    void shouldReturnOkStatusForSwaggerAPIDocsEndpoint() throws Exception {
         mockMvc.perform(get("/v3/api-docs"))
                 .andExpect(status().isOk());
     }
