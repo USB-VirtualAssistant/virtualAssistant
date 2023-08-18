7781b4b8a9ddf9b924ab23f31ac7367a282be066
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-08-17 22:40:21 -0400

    [gabrielagonzales] added clients to RequestEntityController

--- src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
+++ src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
@@ -1,10 +1,12 @@
 package org.fundacionjala.virtualassistant.textrequest.controller;
 
+import lombok.AllArgsConstructor;
+import org.fundacionjala.virtualassistant.mongo.services.RecordingService;
 import org.fundacionjala.virtualassistant.textrequest.controller.request.TextRequest;
 import org.fundacionjala.virtualassistant.textrequest.controller.response.TextRequestResponse;
 import org.fundacionjala.virtualassistant.textrequest.exception.TextRequestException;
 import org.fundacionjala.virtualassistant.textrequest.service.TextRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.fundacionjala.virtualassistant.whisper.client.WhisperClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +17,11 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/textRequest")
+@AllArgsConstructor
 public class RequestEntityController {
-    @Autowired
     TextRequestService requestEntityService;
+    WhisperClient whisperClient;
+    RecordingService recordingService;
 
     @PostMapping
     public ResponseEntity<TextRequestResponse> createTextRequest(@RequestBody TextRequest textRequest) throws TextRequestException {
