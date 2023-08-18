65a1e7398da387ca1afee0638fdf28d2f7739ff4
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-18 16:35:01 -0400

    ISSUE [santihsdev]: Mock OpenAiService to decouple dependencies on the api

--- src/test/java/org/fundacionjala/virtualassistant/openai/OpenAIClientTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/openai/OpenAIClientTest.java
@@ -1,19 +1,55 @@
 package org.fundacionjala.virtualassistant.openai;
 
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.service.OpenAiService;
 import org.fundacionjala.virtualassistant.clients.openai.OpenAIClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class OpenAIClientTest {
 
-    @Test
-    void givenServiceReturnsText_WhenChatIsCalled_ThenResultIsNotNullOrEmpty() {
-        OpenAIClient client = new OpenAIClient();
-        String result = client.chat("hi how are you");
-        assertTrue(isStringNotNullOrEmpty(result), "result should not be null or empty");
+    private OpenAiService openAiService;
+    private OpenAIClient openAIClient;
+    private CompletionRequest completionRequest;
+    private CompletionResult completionResult;
+    private CompletionChoice completionChoice;
+    private static final String REQUEST = "hi how are you";
+
+    @BeforeEach
+    void setUp() {
+        openAiService = mock(OpenAiService.class);
+        openAIClient = new OpenAIClient(openAiService);
+        completionRequest = CompletionRequest.builder()
+                .prompt(REQUEST)
+                .model(OpenAIClient.MODEL)
+                .maxTokens(OpenAIClient.MAX_TOKENS)
+                .temperature(OpenAIClient.TEMPERATURE)
+                .echo(OpenAIClient.ECHO)
+                .build();
+
+        completionChoice = new CompletionChoice();
+        completionChoice.setText(REQUEST);
+        completionResult = mock(CompletionResult.class);
     }
-    private boolean isStringNotNullOrEmpty(String input) {
-        return input != null && !input.trim().isEmpty();
+
+    @Test
+    void shouldReturnAStringAndCallOpenAIServiceMethodsWhenARequestIsUseInOpenAIClient() {
+        when(openAiService.createCompletion(completionRequest)).thenReturn(completionResult);
+        when(completionResult.getChoices()).thenReturn(List.of(completionChoice));
+
+        String result = openAIClient.chat(REQUEST);
+
+        verify(openAiService).createCompletion(completionRequest);
+        verify(completionResult).getChoices();
+        assertNotNull(result, "result should not be null");
     }
 }
