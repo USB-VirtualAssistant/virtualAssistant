e75dea068a3ad9240e7c838c8630f34aa08e77b0
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-17 20:24:56 -0400

    VA-032-[Juanzq10Salesiana] Added ocument categorizer example

---
+++ src/main/java/org/fundacionjala/virtualassistant/openNLP/DocumentCategorizerExample.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.virtualassistant.npl;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.tokenize.SimpleTokenizer;
+
+public class DocumentCategorizerExample {
+    public static String getIntent(String userRequest) {
+        InputStream modelIn = null;
+        DoccatModel model = null;
+        try {
+            modelIn = new FileInputStream("src/main/resources/nlp-model/en-trained-model.bin");
+            model = new DoccatModel(modelIn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
+        SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+        String[] tokens = tokenizer.tokenize(userRequest);
+        double[] probabilities = categorizer.categorize(tokens);
+
+        return categorizer.getBestCategory(probabilities);
+    }
+
+}
