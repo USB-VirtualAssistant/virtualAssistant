1d65d266bf6fbe80192ee7d3450fac8979575902
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-08-18 12:11:33 -0400

    solve test for openAIClient

--- src/test/java/org/fundacionjala/virtualassistant/openai/OpenAIClientTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/openai/OpenAIClientTest.java
@@ -1,42 +1,19 @@
 package org.fundacionjala.virtualassistant.openai;
 
-import com.theokanning.openai.completion.CompletionChoice;
-import com.theokanning.openai.completion.CompletionResult;
-import com.theokanning.openai.service.OpenAiService;
 import org.fundacionjala.virtualassistant.clients.openai.OpenAIClient;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class OpenAIClientTest {
 
-    private OpenAIClient client;
-    private OpenAiService service;
-
-    @BeforeEach
-    void setUp() {
-        service = mock(OpenAiService.class);
-        client = new OpenAIClient();
-    }
-
     @Test
-    public void givenServiceReturnsText_WhenChatIsCalled_ThenResultIsNotNullOrEmpty() {
-        CompletionChoice choice = new CompletionChoice();
-        choice.setText("Hello world");
-        CompletionResult completion = new CompletionResult();
-        completion.setChoices(Collections.singletonList(choice));
-        when(service.createCompletion(any())).thenReturn(completion);
-        String result = client.chat("Hi");
-
+    void givenServiceReturnsText_WhenChatIsCalled_ThenResultIsNotNullOrEmpty() {
+        OpenAIClient client = new OpenAIClient();
+        String result = client.chat("hi how are you");
         assertTrue(isStringNotNullOrEmpty(result), "result should not be null or empty");
     }
-    public boolean isStringNotNullOrEmpty(String input) {
+    private boolean isStringNotNullOrEmpty(String input) {
         return input != null && !input.trim().isEmpty();
     }
-
 }
