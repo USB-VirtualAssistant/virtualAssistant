89e5952b79965f143d059dcc5a261a893679398c
melanyArgandona <melmune@gmail.com>
2023-08-21 09:37:34 -0400

    [VA-039][melanyArgandona2002]modified comments for the first pr, separate statis methods for no statics and create a constructor

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/OpenAIClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/OpenAIClient.java
@@ -4,20 +4,19 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 public class OpenAIClient implements AIClientInterface {
     private static final Dotenv dotenv = Dotenv.load();
-    private static final String TOKEN = dotenv.get("TOKEN_AICLIENTE");
+    private static final String TOKEN = dotenv.get("TOKEN_AICLIENT");
     public static final String MODEL = "text-davinci-003";
     public static final int MAX_TOKENS = 1000;
     public static final double TEMPERATURE = 0.8;
     public static final boolean ECHO = true;
+
     private OpenAiService service;
 
-    public OpenAIClient(OpenAiService service) {
-        this.service = service;
+    public OpenAIClient() {
+        this.service = new OpenAiService(TOKEN);;
     }
 
     @Override
