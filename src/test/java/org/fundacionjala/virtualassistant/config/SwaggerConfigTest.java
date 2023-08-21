abf620da997670e25ed17ffef332dd5933394095
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-21 10:42:29 -0400

    ISSUE 59 [santihsdev]: Update test not to use docker and .env

--- src/test/java/org/fundacionjala/virtualassistant/config/SwaggerConfigTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/config/SwaggerConfigTest.java
@@ -1,39 +1,35 @@
 package org.fundacionjala.virtualassistant.config;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 class SwaggerConfigTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-        String postgresUser = System.getenv("POSTGRES_USER");
-        String postgresPassword = System.getenv("POSTGRES_PASSWORD");
-        String mongoUsername = System.getenv("MONGO_INITDB_ROOT_USERNAME");
-        String mongoPassword = System.getenv("MONGO_INITDB_ROOT_PASSWORD");
-
-        System.setProperty("POSTGRES_USER", postgresUser);
-        System.setProperty("POSTGRES_PASSWORD", postgresPassword);
-        System.setProperty("MONGO_INITDB_ROOT_USERNAME", mongoUsername);
-        System.setProperty("MONGO_INITDB_ROOT_PASSWORD", mongoPassword);
-    }
-
     @Test
     void shouldReturnOkStatusForSwaggerUIEndpoint() throws Exception {
         mockMvc.perform(get("/swagger-ui/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnNotFoundStatusForSwaggerUIEndpoint() throws Exception {
+        mockMvc.perform(get("/swagger-u/"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
