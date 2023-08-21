c9dda2ba927ece26eefcb8dffdb7398a7c8a26e1
melanyArgandona <melmune@gmail.com>
2023-08-21 18:03:06 -0400

    [VA-39][melanyArgandona2002] convert method removepatternfromstart in private

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/AIClientInterface.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/AIClientInterface.java
@@ -2,6 +2,4 @@ package org.fundacionjala.virtualassistant.clients.openai;
 
 public interface AIClientInterface {
     String chat(String request);
-
-    String removePatternFromStart(StringBuilder input, String toRemovePattern);
 }
