13cb5aa2f543a56a1eabe7c06b8dc5f8973de0df
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-08-21 09:55:36 -0400

    [gabrielagonzales] Fix imports in build.gradle and add query for create keyspace

--- src/main/java/org/fundacionjala/virtualassistant/cassandra/Connection.java
+++ src/main/java/org/fundacionjala/virtualassistant/cassandra/Connection.java
@@ -9,8 +9,8 @@ public class Connection {
         Session session;
 
         cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-        session = cluster.connect("cassandra_keyspace");
-
-        session.execute("INSERT INTO cassandra_keyspace.shopping_cart (userid, item_count, last_update_timestamp)VALUES ('9876', 2, toTimeStamp(now()));");
+        session = cluster.connect();
+        session.execute("CREATE KEYSPACE IF NOT EXISTS cassandra_keyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : '1' };");
+        session.execute("CREATE TABLE IF NOT EXISTS cassandra_keyspace.audio_files (audio_id uuid PRIMARY KEY,audioFile blob);");
     }
 }
