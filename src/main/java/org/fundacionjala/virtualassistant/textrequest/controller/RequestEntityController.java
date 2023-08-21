d96c1971e9e71425f9f677386e207422ec09fa16
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-08-21 10:32:34 -0400

    VA-012[desireeacosta]: Changed return of getTextRequests method

--- src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
+++ src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
@@ -6,6 +6,7 @@ import org.fundacionjala.virtualassistant.textrequest.controller.response.TextRe
 import org.fundacionjala.virtualassistant.textrequest.exception.TextRequestException;
 import org.fundacionjala.virtualassistant.textrequest.service.TextRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/textRequest")
@@ -34,6 +37,7 @@ public class RequestEntityController {
     @GetMapping("/{userId}")
     public List<RequestEntity> getTextRequests(@NotNull @PathVariable Long userId,
                                                @NotNull @RequestParam(name = "id_context") Long contextID) {
-        return requestEntityService.getTextRequestByUserAndContext(userId, contextID);
+        List<RequestEntity> textRequests = requestEntityService.getTextRequestByUserAndContext(userId, contextID);
+        return new ResponseEntity<>(textRequests, OK).getBody();
     }
 }
