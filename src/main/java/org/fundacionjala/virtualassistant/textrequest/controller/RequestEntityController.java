e131e8eb26948947d9325b79c17c8b7fec013c03
Ariel  Claros <ariel.claros.60091@usalesiana.edu.bo>
2023-08-21 09:28:17 -0400

    VA012[ArielClaros]: Deleted unnecessary line breaks between imports in the RequestEntityController

--- src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
+++ src/main/java/org/fundacionjala/virtualassistant/textrequest/controller/RequestEntityController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
