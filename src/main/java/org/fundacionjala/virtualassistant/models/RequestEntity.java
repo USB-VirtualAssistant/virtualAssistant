4ecdacf3dc436cd3d3a6e25c8e843fb95815c740
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-08-21 08:53:56 -0400

    Removed not used imports

--- src/main/java/org/fundacionjala/virtualassistant/models/RequestEntity.java
+++ src/main/java/org/fundacionjala/virtualassistant/models/RequestEntity.java
@@ -6,16 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.ZonedDateTime;
-import java.util.Date;
 
 @Entity
 @Data
