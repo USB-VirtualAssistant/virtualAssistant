65fb5a92481e0a1b92505a589a435744fd9885a8
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-08-22 19:05:08 -0400

    VA-034[valeriavalenz]: Deleted autowired annotation and added AllArgsConstructor annotation in OpenAiComponentImpl

--- src/main/java/org/fundacionjala/virtualassistant/openai/component/OpenAIComponentImp.java
+++ src/main/java/org/fundacionjala/virtualassistant/openai/component/OpenAIComponentImp.java
@@ -1,16 +1,14 @@
 package org.fundacionjala.virtualassistant.openai.component;
 
+import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.clients.openai.AIClientInterface;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class OpenAIComponentImp implements OpenAIComponent {
 
     private AIClientInterface openai;
-
-    public OpenAIComponentImp(AIClientInterface openai) {
-        this.openai = openai;
-    }
 
     public String getResponse(String message) {
         return openai.chat(message);
