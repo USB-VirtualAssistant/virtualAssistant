bebf9bc2c93c607e1de684b16b2e4ed48dd7115f
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-08-22 22:42:43 -0400

    Removed unused constants

--- src/test/java/org/fundacionjala/virtualassistant/request/controller/TextRequestControllerTest.java
+++ src/test/java/org/fundacionjala/virtualassistant/request/controller/TextRequestControllerTest.java
@@ -4,8 +4,6 @@ import org.fundacionjala.virtualassistant.models.RequestEntity;
 import org.fundacionjala.virtualassistant.textrequest.controller.RequestEntityController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +13,10 @@ import static org.mockito.Mockito.when;
 
 public class TextRequestControllerTest {
     private RequestEntityController textRequestController;
-
-    private static final String REQUEST_TEXT_A = "How's the weather today?";
-    private static final String REQUEST_TEXT_B = "Hello assistant!";
-    private static final String ID_AUDIO_A = "mongo_id";
-    private static final String ID_AUDIO_B = "mongo_id";
     private static final Long CONTEXT_ID_1 = 1L;
     private static final Long USER_ID_1 = 2L;
     private static final Long CONTEXT_ID_2 = 2L;
     private static final Long USER_ID_2 = 1L;
-    private static final ZonedDateTime REQUEST_DATE = ZonedDateTime.of(2023, 8, 2, 12,
-            0, 0, 0, ZoneId.systemDefault());
     private RequestEntity requestA;
     private RequestEntity requestB;
 
