1b73a6406fb52ecf6665c1cd3b4ad8032e6e28f3
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-23 19:15:24 -0400

    VA-032 [@Juanzq10Salesiana] Added the sign of the methods

---
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/GetAlbumsAction.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.virtualassistant.taskhandler;
+
+public class GetAlbumsAction implements TaskAction{
+    @Override
+    public String handleAction() {
+        return null;
+    }
+}
