6225e3330267e1af9be9ab719e392bca21f77ffa
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-08-22 22:44:25 -0400

    VA-012 02[desireeacosta]: Removed line break at imports

--- src/main/java/org/fundacionjala/virtualassistant/service/TextRequestServiceImpl.java
+++ src/main/java/org/fundacionjala/virtualassistant/service/TextRequestServiceImpl.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.models.RequestEntity;
 import org.fundacionjala.virtualassistant.repository.RequestEntityRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class TextRequestServiceImpl implements TextRequestService{
