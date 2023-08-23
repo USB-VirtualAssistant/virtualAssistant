38b02f33cb667ca34256560955759c6ec8c1ecb1
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-23 18:51:54 -0400

    VA-016[@Juanzq10Salesiana] Added-package and first class

---
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/Proxy.java
@@ -0,0 +1,9 @@
+package org.fundacionjala.virtualassistant.taskhandler;
+
+import org.fundacionjala.virtualassistant.player.spotify.client.SpotifyClient;
+import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
+
+public class Proxy {
+    MusicService musicService = new MusicService(new SpotifyClient());
+
+}
