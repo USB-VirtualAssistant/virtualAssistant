3e716624471a16c4b0b58c94fa0d3445ad8a6c69
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-08-24 09:44:19 -0400

    VA-34[valeriavalenz]: Updated code based on the new OpenAi implementation

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/component/OpenAIComponentImp.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/component/OpenAIComponentImp.java
@@ -1,17 +1,16 @@
-package org.fundacionjala.virtualassistant.openai.component;
+package org.fundacionjala.virtualassistant.clients.openai.component;
 
 import lombok.AllArgsConstructor;
-import org.fundacionjala.virtualassistant.clients.openai.AIClientInterface;
+import org.fundacionjala.virtualassistant.clients.openai.service.ChatService;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class OpenAIComponentImp implements OpenAIComponent {
 
-    private AIClientInterface openai;
+    private ChatService service;
 
     public String getResponse(String message) {
-        return openai.chat(message);
+        return service.chat(message);
     }
-
 }
