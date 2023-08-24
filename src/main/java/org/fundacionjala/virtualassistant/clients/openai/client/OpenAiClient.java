c2e368248c5a5047ce7a95be965747abe7cf4127
melanyArgandona <melmune@gmail.com>
2023-08-23 22:57:58 -0400

    [VA-039][melanyArgandona2022]added tokento read to de properties

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/client/OpenAiClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/client/OpenAiClient.java
@@ -2,17 +2,25 @@ package org.fundacionjala.virtualassistant.clients.openai.client;
 
 import com.theokanning.openai.completion.CompletionRequest;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:openAi.properties")
 @NoArgsConstructor
 public class OpenAiClient implements AiClient{
     public static final String MODEL = "text-davinci-003";
     public static final int MAX_TOKENS = 1000;
     public static final double TEMPERATURE = 0.8;
     public static final boolean ECHO = true;
-    public static final String TOKEN = "my_token";
+
+    @Value("${openAi.client.token}")
+    private String openAiToken;
 
     @Override
     public String getToken() {
-        return TOKEN;
+        return openAiToken;
     }
 
     @Override
