db619904048b449bfbaea684b5054731a6f0a19c
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-08-24 18:32:12 -0400

    VA-016[@PabloTercerosCC]: Music Actions Implementation

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/GetAlbumsAction.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/GetAlbumsAction.java
@@ -1,16 +1,17 @@
-package org.fundacionjala.virtualassistant.taskhandler;
+package org.fundacionjala.virtualassistant.taskhandler.actions;
 
 import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
+import org.fundacionjala.virtualassistant.taskhandler.TaskAction;
 
-public class GetAlbumsAction implements TaskAction{
+public class GetAlbumsAction implements TaskAction {
     private MusicService musicService;
 
     public GetAlbumsAction(MusicService musicService) {
         this.musicService = musicService;
     }
+
     @Override
     public String handleAction() {
-        // TO DO:
-        return musicService.toString();
+        return musicService.getUserSavedAlbums().getBody();
     }
 }
