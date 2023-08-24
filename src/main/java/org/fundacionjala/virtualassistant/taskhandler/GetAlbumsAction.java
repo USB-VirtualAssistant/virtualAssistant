87355bdf11b60ff77cc638482c6c3faad02f7b1c
Juan_Zurita_10 <juan.zurita.60112@usalesiana.edu.bo>
2023-08-23 20:25:52 -0400

    VA-032 [@Juanzq10Salesiana] first implementation of proxy done

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/GetAlbumsAction.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/GetAlbumsAction.java
@@ -1,8 +1,16 @@
 package org.fundacionjala.virtualassistant.taskhandler;
 
+import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
+
 public class GetAlbumsAction implements TaskAction{
+    private MusicService musicService;
+
+    public GetAlbumsAction(MusicService musicService) {
+        this.musicService = musicService;
+    }
     @Override
     public String handleAction() {
-        return null;
+        // TO DO:
+        return musicService.toString();
     }
 }
