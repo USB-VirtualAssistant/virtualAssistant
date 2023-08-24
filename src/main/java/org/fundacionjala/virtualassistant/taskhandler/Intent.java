f4b3ac1ec0a1cff00eb122be0374a6512076a8de
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-08-24 18:33:10 -0400

    VA-016[@PabloTercerosCC]: Music Intents Implementation

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/Intent.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/Intent.java
@@ -1,7 +1,17 @@
 package org.fundacionjala.virtualassistant.taskhandler;
 
+import org.fundacionjala.virtualassistant.taskhandler.actions.*;
+
 public enum Intent {
-    GET_ALBUMS(new GetAlbumsAction(SingletonServices.getMusicService()));
+
+    GET_ALBUMS(new GetAlbumsAction(SingletonServices.getMusicService())),
+    GET_TRACKS(new GetTracksAction(SingletonServices.getMusicService())),
+    GET_FOLLOWING(new GetFollowingAction(SingletonServices.getMusicService())),
+    GET_PLAYER(new GetPlayerAction(SingletonServices.getMusicService())),
+    PAUSE(new PauseAction(SingletonServices.getMusicService())),
+    NEXT(new NextAction(SingletonServices.getMusicService())),
+    PREVIOUS(new PreviousAction(SingletonServices.getMusicService())),
+    CONTINUE(new ContinueAction(SingletonServices.getMusicService()));
 
     private TaskAction taskAction;
 
