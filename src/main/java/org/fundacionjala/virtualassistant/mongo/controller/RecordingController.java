85ba5aec24262c32dddb0d857b26d6e79cfd7b09
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-08-23 20:48:42 -0400

    VA-040 [santihsdev]: Add validation in recording request, update endpoint names and update tests

--- src/main/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingController.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/RecordingController.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.Optional;
@@ -35,24 +36,25 @@ public class RecordingController {
         return new ResponseEntity<>(recordings, HttpStatus.OK);
     }
 
-    @GetMapping("/audio/{id}")
-    public ResponseEntity<RecordingResponse> getRecordingID(@PathVariable("id") String id) throws RecordingException {
+    @GetMapping("/{id}")
+    public ResponseEntity<RecordingResponse> getRecordingID(@NotEmpty @PathVariable("id") String id)
+            throws RecordingException {
         Optional<RecordingResponse> recording = Optional.ofNullable(recordingService.getRecording(id));
-        if (recording.isPresent()){
-            return new ResponseEntity<>(recording.get(),HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return recording.map(recordingResponse -> new ResponseEntity<>(recordingResponse, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
-    @GetMapping("/audio/")
-    public ResponseEntity<List<RecordingResponse>> getRecordingsUser(@RequestParam("idUser") Long idUser, @RequestParam("idChat") Long idChat) throws RecordingException {
+    @GetMapping("/user/{idUser}/chat/{idChat}")
+    public ResponseEntity<List<RecordingResponse>> getRecordingsUser(@NotEmpty @PathVariable("idUser") Long idUser,
+                                                                     @NotEmpty @PathVariable("idChat") Long idChat)
+            throws RecordingException {
         List<RecordingResponse> recordings = recordingService.getAllRecordingsToUser(idUser, idChat);
         return new ResponseEntity<>(recordings, HttpStatus.OK);
     }
 
-    @PostMapping("/")
-    public ResponseEntity<RecordingResponse> saveRecording(@ModelAttribute RecordingRequest recordingRequest) throws RecordingException {
+    @PostMapping
+    public ResponseEntity<RecordingResponse> saveRecording(@Valid @ModelAttribute RecordingRequest recordingRequest)
+            throws RecordingException {
         RecordingResponse savedRecording = recordingService.saveRecording(recordingRequest);
         return new ResponseEntity<>(savedRecording, HttpStatus.CREATED);
     }
