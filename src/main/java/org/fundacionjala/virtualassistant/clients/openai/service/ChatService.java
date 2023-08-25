92f5915a9dda908314a9a3c1a4f13f0e4a865df3
melanyArgandona <melmune@gmail.com>
2023-08-25 10:03:26 -0400

    [VA-039][melanyArgandona2002]modifed class for comments

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/service/ChatService.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/service/ChatService.java
@@ -1,34 +1,40 @@
 package org.fundacionjala.virtualassistant.clients.openai.service;
 
+
 import com.theokanning.openai.service.OpenAiService;
 import org.fundacionjala.virtualassistant.clients.openai.client.OpenAiClient;
+
+import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class ChatService {
     private OpenAiClient openAiClient;
+    private OpenAiService openAiService;
 
     public ChatService(OpenAiClient openAiClient){
         this.openAiClient = openAiClient;
+        this.openAiService = new OpenAiService(openAiClient.getToken());
     }
 
     @NotEmpty
     @Pattern(regexp = ".*[a-zA-Z].*")
     public String chat(String request) {
-        OpenAiService service = new OpenAiService(openAiClient.getToken());
+        String answer = generateChatAnswer(this.openAiService, request);
+        return removePatternFromStart(answer, openAiClient.buildCompletionRequest(request).getPrompt());
+    }
 
-        StringBuilder answerBuilder = new StringBuilder();
-        service.createCompletion(openAiClient.buildCompletionRequest(request))
+    private String generateChatAnswer(OpenAiService service, String request) {
+        return service.createCompletion(openAiClient.buildCompletionRequest(request))
                 .getChoices()
-                .forEach(choice -> answerBuilder.append(choice.getText()));
-        return removePatternFromStart(answerBuilder, openAiClient.buildCompletionRequest(request).getPrompt());
+                .stream()
+                .map(choice -> choice.getText())
+                .collect(Collectors.joining());
     }
 
     private String removePatternFromStart(StringBuilder input, String toRemovePattern) {
         int patternFinishIndex = toRemovePattern.length();
         return input.substring(patternFinishIndex).trim();
-    }
-    public void setOpenAiService(OpenAiService openAiService) {
     }
 }
