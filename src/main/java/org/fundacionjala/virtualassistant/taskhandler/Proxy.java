a719217ad9243d29f4b49e92ebb79df86edc5afc
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-24 22:45:38 -0400

    [VA-016:@SebaRosaleSZ]: Code improvements on Proxy class

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/Proxy.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/Proxy.java
@@ -1,8 +1,5 @@
 package org.fundacionjala.virtualassistant.taskhandler;
 
-import org.fundacionjala.virtualassistant.player.spotify.client.SpotifyClient;
-import org.fundacionjala.virtualassistant.player.spotify.service.MusicService;
-
 public class Proxy {
     public String handleIntent(String userIntent) {
         Intent enumIntent = Intent.valueOf(userIntent);
