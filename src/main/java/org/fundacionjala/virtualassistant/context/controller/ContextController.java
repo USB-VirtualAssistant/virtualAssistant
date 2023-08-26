a9737d9f590c61945c9d182569239025f212d2b3
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-25 23:26:47 -0400

    VA-44[SerranoAlejandroSelaya]: add context controller, service and repository

---
+++ src/main/java/org/fundacionjala/virtualassistant/context/controller/ContextController.java
@@ -0,0 +1,39 @@
+package org.fundacionjala.virtualassistant.context.controller;
+
+import lombok.NonNull;
+import org.fundacionjala.virtualassistant.context.controller.Response.ContextResponse;
+import org.fundacionjala.virtualassistant.context.service.ContextService;
+import org.fundacionjala.virtualassistant.models.ContextEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/context/")
+public class ContextController {
+    ContextService contextService;
+
+    public ContextController(ContextService contextService){
+        this.contextService =  contextService;
+    }
+
+    @GetMapping("{idUser}")
+    public List<ContextResponse> getContextByUser(@NonNull @PathVariable("idUser") Long idUser){
+        System.out.println(idUser);
+        List<ContextResponse> contexts = contextService.findContextByUserId(idUser);
+        return new ResponseEntity<>(contexts, HttpStatus.OK).getBody();
+    }
+
+    @PostMapping()
+    public ContextEntity getContextByUser(@NotEmpty @RequestBody ContextResponse request){
+        ContextEntity context = contextService.saveContext(request);
+        return new ResponseEntity<>(context, HttpStatus.OK).getBody();
+    }
+
+
+}
