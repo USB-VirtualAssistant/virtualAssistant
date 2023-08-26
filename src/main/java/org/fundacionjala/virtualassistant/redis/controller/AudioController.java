809416f87dab4249405398cc8eea961f86a7d455
Gabriela Gonzales Villarroel <gabriela.gonzales.60113@usalesiana.edu.bo>
2023-08-25 21:33:33 -0400

    VA-035[gabrielagonzales] Rebase

--- src/main/java/org/fundacionjala/virtualassistant/redis/controller/AudioController.java
+++ src/main/java/org/fundacionjala/virtualassistant/redis/controller/AudioController.java
@@ -1,25 +1,32 @@
 package org.fundacionjala.virtualassistant.redis.controller;
 
 import org.fundacionjala.virtualassistant.redis.entity.Audio;
+import org.fundacionjala.virtualassistant.redis.exception.FileSaveException;
+import org.fundacionjala.virtualassistant.redis.exception.RedisDataNotFoundException;
 import org.fundacionjala.virtualassistant.redis.service.AudioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/audio")
+@RequestMapping("/audio/")
 public class AudioController {
 
     @Autowired
     private AudioService audioService;
 
-    @PostMapping("/add")
-    public Audio addAudio(@RequestParam("file") MultipartFile file) {
+    @PostMapping("add")
+    public Audio addAudio(@RequestParam("file") MultipartFile file) throws FileSaveException {
         return audioService.saveFile(file);
     }
 
-    @GetMapping("/get/{id}")
-    public byte[] getAudio(@PathVariable String id) {
+    @GetMapping("get/{id}")
+    public byte[] getAudio(@PathVariable String id) throws RedisDataNotFoundException {
         return audioService.findFileById(id);
     }
 }
--- src/main/java/org/fundacionjala/virtualassistant/redis/entity/Audio.java
+++ src/main/java/org/fundacionjala/virtualassistant/redis/entity/Audio.java
@@ -8,7 +8,6 @@ import org.springframework.data.redis.core.RedisHash;
 @RedisHash("Audio")
 public class Audio {
     private String id;
-    @Getter
     private byte[] audioFile;
 
     public void setAudioFile(byte[] audioFile) {
