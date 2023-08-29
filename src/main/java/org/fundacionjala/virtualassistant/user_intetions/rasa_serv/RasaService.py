2d9c2bc5275b3dec46679a486233cbc935cce84d
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-28 22:45:23 -0400

    [VA-032@SebaRosalesZ]:Review comment correction:initialize_flask_app moved to rest client

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/rasa_serv/RasaService.py
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/rasa_serv/RasaService.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, jsonify
-import requests
 import subprocess
 import threading
+from flask import Flask, request, jsonify
+import requests
 
 MICROSERVICE_PORT = 8082
 RASA_PORT = 3333
 HOST_ADDRESS = '0.0.0.0'
-RASA_URL = f"http://localhost:{RASA_PORT}/model/parse"
 CONSUME_ROUTE = '/consume-rasa'
 MODEL_PATH = "models/"
+TEXT_KEY = 'text'
+RASA_URL = f"http://localhost:{RASA_PORT}/model/parse"
 
 class RasaRunner:
     def __init__(self, model_path, port=3333):
@@ -30,32 +31,23 @@ class RasaRunner:
         except subprocess.CalledProcessError as e:
             print("An error occurred:", e)
 
-class RasaService:
-    def __init__(self, rasa_port, microservice_port):
-        self.rasa_port = rasa_port
-        self.microservice_port = microservice_port
-        self.rasa_runner = RasaRunner(model_path=MODEL_PATH, port=rasa_port)
-
-    def start_services(self):
-        rasa_thread = threading.Thread(target=self.rasa_runner.start_rasa_server)
-        rasa_thread.start()
-        
-        self.app.run(host=HOST_ADDRESS, port=self.microservice_port)
+class RestClient:
+    def __init__(self, app):
+        self.app = app
 
     def initialize_flask_app(self):
-        self.app = Flask(__name__)
         self.app.route(CONSUME_ROUTE, methods=['POST'])(self.consume_rasa)
 
     def consume_rasa(self):
         try:
             request_data = request.json
-            input_text = request_data.get('text', '')
+            input_text = request_data.get(TEXT_KEY, '')
 
             if not input_text:
                 return jsonify({"error": "Text not provided."}), 400
 
             payload = {
-                "text": input_text
+                TEXT_KEY: input_text
             }
 
             response = requests.post(RASA_URL, json=payload)
@@ -69,7 +61,22 @@ class RasaService:
         except Exception as e:
             return jsonify({"error": "An error occurred.", "details": str(e)}), 500
 
+class RasaService:
+    def __init__(self, rasa_port, microservice_port):
+        self.rasa_port = rasa_port
+        self.microservice_port = microservice_port
+        self.rasa_runner = RasaRunner(model_path=MODEL_PATH, port=rasa_port)
+
+    def start_services(self):
+        rasa_thread = threading.Thread(target=self.rasa_runner.start_rasa_server)
+        rasa_thread.start()
+
+        app = Flask(__name__)
+        rest_client = RestClient(app)
+        rest_client.initialize_flask_app()
+
+        app.run(host=HOST_ADDRESS, port=self.microservice_port)
+
 if __name__ == '__main__':
     microservice = RasaService(rasa_port=RASA_PORT, microservice_port=MICROSERVICE_PORT)
-    microservice.initialize_flask_app()
     microservice.start_services()
