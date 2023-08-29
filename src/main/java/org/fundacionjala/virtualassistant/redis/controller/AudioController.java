dbdd22a2dc7f8b754ba424b705431b3d5ceb26b7
Gabriela Gonzales Villarroel <gabriela.gonzales.60113@usalesiana.edu.bo>
2023-08-28 23:08:09 -0400

    VA-035[@gabrielagonzalesv7]: Fixed comments and fix tests for AudioService

--- src/main/java/org/fundacionjala/virtualassistant/redis/controller/AudioController.java
+++ src/main/java/org/fundacionjala/virtualassistant/redis/controller/AudioController.java
@@ -4,7 +4,6 @@ import org.fundacionjala.virtualassistant.redis.entity.Audio;
 import org.fundacionjala.virtualassistant.redis.exception.FileSaveException;
 import org.fundacionjala.virtualassistant.redis.exception.RedisDataNotFoundException;
 import org.fundacionjala.virtualassistant.redis.service.AudioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/audio/")
 public class AudioController {
-
-    @Autowired
     private AudioService audioService;
+
+    public AudioController(AudioService audioService) {
+        this.audioService = audioService;
+    }
 
     @PostMapping("add")
     public Audio addAudio(@RequestParam("file") MultipartFile file) throws FileSaveException {
