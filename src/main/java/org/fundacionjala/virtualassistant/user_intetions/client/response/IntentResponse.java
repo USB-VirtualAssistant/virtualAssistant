b890676d52cba45875c314303a69bdd6023a3a5e
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-29 18:59:03 -0400

    [VA-032:SebaRosalesZ]Added IntentResponse

---
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentResponse.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.virtualassistant.user_intetions.client.response;
+
+import java.util.List;
+
+public class IntentResponse {
+     List<IntentEntity> intentEntities;
+     Intent intent;
+}
