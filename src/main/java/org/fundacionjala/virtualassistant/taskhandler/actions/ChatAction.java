31f18dc58b5a256857105f16a1847bc4c2bae6fb
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-08-30 18:28:22 -0400

    VA-022[@PabloTercerosCC]: OpenAI Intent Implementation

---
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/ChatAction.java
@@ -0,0 +1,16 @@
+package org.fundacionjala.virtualassistant.taskhandler.actions;
+
+import lombok.AllArgsConstructor;
+import org.fundacionjala.virtualassistant.clients.openai.service.ChatService;
+import org.fundacionjala.virtualassistant.taskhandler.TaskAction;
+
+@AllArgsConstructor
+public class ChatAction implements TaskAction {
+
+    private ChatService chatService;
+
+    @Override
+    public String handleAction() {
+        return chatService.chat("text");
+    }
+}
