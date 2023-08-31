8ed26b34d8b3a341cea651a13efeace16f915a25
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-08-30 20:01:47 -0400

    [VA-032:@SebaRosalesZ]Update over UserIntentsClient interface

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/repository/UserIntentsClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/repository/UserIntentsClient.java
@@ -1,7 +1,8 @@
 package org.fundacionjala.virtualassistant.user_intetions.repository;
 
+import org.fundacionjala.virtualassistant.user_intetions.client.response.IntentResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface UserIntentsClient {
-    ResponseEntity<String> processUserIntentsByMicroService(String input);
+    ResponseEntity<IntentResponse> processUserIntentsByMicroService(String input);
 }
