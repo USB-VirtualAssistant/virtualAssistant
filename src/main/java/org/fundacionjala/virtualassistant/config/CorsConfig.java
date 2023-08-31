be26e6fb6e3a04430c87e4b2e0f2856d89c0b893
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-08-31 10:52:24 -0400

    VA-044[SerranoAlejandroSelaya]: correction of the comments of august 30th

--- src/main/java/org/fundacionjala/virtualassistant/config/CorsConfig.java
+++ src/main/java/org/fundacionjala/virtualassistant/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.virtualassistant.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Adjust this as needed
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
