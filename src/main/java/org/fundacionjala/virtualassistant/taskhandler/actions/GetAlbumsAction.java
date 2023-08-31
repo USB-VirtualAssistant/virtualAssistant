063ac2d966b841b79cec4d6f6e350a6ac150eeae
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-08-31 00:41:28 -0400

    VA-022[@PabloTercerosCC]: Added Entity Argument

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/GetAlbumsAction.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/GetAlbumsAction.java
@@ -11,7 +11,7 @@ public class GetAlbumsAction implements TaskAction {
     }
 
     @Override
-    public String handleAction() {
+    public String handleAction(String intent) {
         return musicService.getUserSavedAlbums().getBody();
     }
 }
