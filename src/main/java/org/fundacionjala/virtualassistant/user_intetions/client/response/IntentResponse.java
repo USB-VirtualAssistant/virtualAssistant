0e53305ebc37aa0bc16a6c3a7299d786960d425c
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-09-01 11:41:19 -0400

    [VA-022:SebaRosalesZ] Improved Proxy constructor for Rasa integration

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentResponse.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentResponse.java
@@ -1,7 +1,9 @@
 package org.fundacionjala.virtualassistant.user_intetions.client.response;
 
+import lombok.Getter;
 import java.util.List;
 
+@Getter
 public class IntentResponse {
      List<IntentEntity> intentEntities;
      Intent intent;
