24a31366eb78e8aa63693c03c8d50f8671ea8a08
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-09-01 11:43:12 -0400

    [VA-022:SebaRosalesZ] Added doRasa method for consudem rasa

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/Proxy.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/Proxy.java
@@ -1,33 +1,42 @@
 package org.fundacionjala.virtualassistant.taskhandler;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.fundacionjala.virtualassistant.taskhandler.exception.IntentException;
 import org.fundacionjala.virtualassistant.taskhandler.factory.IntentFactory;
 import org.fundacionjala.virtualassistant.taskhandler.factory.TaskActionManagerFactory;
 import org.fundacionjala.virtualassistant.taskhandler.factory.TaskActionFactory;
 import org.fundacionjala.virtualassistant.taskhandler.intents.Intent;
 import org.fundacionjala.virtualassistant.taskhandler.intents.IntentManager;
+import org.fundacionjala.virtualassistant.user_intetions.client.RasaClient;
+import org.fundacionjala.virtualassistant.user_intetions.client.response.IntentResponse;
+import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
+@Component
 public class Proxy {
 
     private TaskActionManagerFactory taskActionManagerFactory;
     private IntentFactory intentFactory;
+    private RasaClient rasaClient;
+
+    public Proxy(TaskActionManagerFactory taskActionManagerFactory, IntentFactory intentFactory, RasaClient rasaClient) {
+        this.taskActionManagerFactory = taskActionManagerFactory;
+        this.intentFactory = intentFactory;
+        this.rasaClient = rasaClient;
+    }
 
     public String handleIntent(String userIntent) throws IntentException {
+
         return handleIntent(userIntent, "");
     }
 
-    public String handleIntent(String userIntent, String intentEntity) throws IntentException {
+    public String handleIntent(String userIntent, IntentResponse intentEntity) throws IntentException {
         TaskActionFactory taskActionFactory = taskActionManagerFactory.getTaskActionFactory(userIntent);
-
         Intent intent = taskActionManagerFactory.getIntent(userIntent);
         IntentManager intentManager = intentFactory.getSpecific(intent);
 
         return taskActionFactory
                 .createTaskAction(intentManager.processIntent(userIntent))
-                .handleAction(intentEntity);
+                .handleAction(intentEntity.getIntentEntities());
     }
+
 }
