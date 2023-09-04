24a92e9c9a9bd3803b9ff22ed4bbad8345496bd7
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-09-04 19:08:49 -0400

    VA-022[@PabloTercerosCC]: Fixed Cannot Contruct Instance of IntentResponse

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/Intent.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/Intent.java
@@ -2,8 +2,10 @@ package org.fundacionjala.virtualassistant.user_intetions.client.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class Intent {
     double confidence;
--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentResponse.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentResponse.java
@@ -2,9 +2,12 @@ package org.fundacionjala.virtualassistant.user_intetions.client.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class IntentResponse {
      List<IntentEntity> intentEntities;
