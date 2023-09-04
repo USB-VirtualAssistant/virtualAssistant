06f62d749ff18aeb6e7771c4818b3f7a24d5c16d
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-09-04 19:08:49 -0400

    VA-022[@PabloTercerosCC]: Fixed Cannot Contruct Instance of IntentResponse

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentEntity.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentEntity.java
@@ -2,10 +2,12 @@ package org.fundacionjala.virtualassistant.user_intetions.client.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Jacksonized
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class IntentEntity {
     String entity;
