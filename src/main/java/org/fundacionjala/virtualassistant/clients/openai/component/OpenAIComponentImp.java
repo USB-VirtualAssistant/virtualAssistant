40c7e150542dd89d1cc4e42867b5bfe8a6a6567f
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-09-05 22:13:32 -0400

    VA-059:[valeriavalenz] Added Task Handler in OpenAiComponent and added spring boot annotations

--- src/main/java/org/fundacionjala/virtualassistant/clients/openai/component/OpenAIComponentImp.java
+++ src/main/java/org/fundacionjala/virtualassistant/clients/openai/component/OpenAIComponentImp.java
@@ -2,15 +2,21 @@ package org.fundacionjala.virtualassistant.clients.openai.component;
 
 import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.clients.openai.service.ChatService;
+import org.fundacionjala.virtualassistant.taskhandler.TaskHandler;
+import org.fundacionjala.virtualassistant.taskhandler.exception.IntentException;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class OpenAIComponentImp implements OpenAIComponent {
 
-    private ChatService service;
+    private TaskHandler handler;
 
     public String getResponse(String message) {
-        return service.chat(message);
+        try {
+            return handler.handleIntent(message);
+        } catch (IntentException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
