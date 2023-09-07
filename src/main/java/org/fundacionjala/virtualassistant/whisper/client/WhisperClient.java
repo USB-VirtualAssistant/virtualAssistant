368e1be0114ca4c8cc86aa5a5a50bfea62e1c9bc
Gabriela Gonzales Villarroel <gabriela.gonzales.60113@usalesiana.edu.bo>
2023-09-07 10:39:46 -0400

    VA-061[@gabrielagonzalesv7]: Added ASR implementation connection

--- src/main/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClient.java
+++ src/main/java/org/fundacionjala/virtualassistant/whisper/client/WhisperClient.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 
+@Component
 @Data
 public class WhisperClient implements ASRClient {
 
