0f94fe405703abf30d6360d13ea8fc54fc211ed0
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-09-07 09:12:11 -0400

    [VA-032:SebaRosalesZ]:Added UserIntentsClient

--- src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentEntity.java
+++ src/main/java/org/fundacionjala/virtualassistant/user_intetions/client/response/IntentEntity.java
@@ -1,10 +1,12 @@
 package org.fundacionjala.virtualassistant.user_intetions.client.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
 
 @Jacksonized
 @Getter
+@Builder
 public class IntentEntity {
     String entity;
     String value;
