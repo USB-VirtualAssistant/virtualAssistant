0b903214fa80646872364255234fa44e663b1e47
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-09-08 02:27:21 -0400

    VA-060[SerranoAlejandroSelaya]: Add put and delete method in context service and controller

--- src/main/java/org/fundacionjala/virtualassistant/context/service/ContextService.java
+++ src/main/java/org/fundacionjala/virtualassistant/context/service/ContextService.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.context.controller.Request.ContextRequest;
 import org.fundacionjala.virtualassistant.context.controller.Response.ContextResponse;
 import org.fundacionjala.virtualassistant.context.exception.ContextException;
+import org.fundacionjala.virtualassistant.context.exception.ContextNotFoundException;
+import org.fundacionjala.virtualassistant.context.exception.ContextRequestException;
 import org.fundacionjala.virtualassistant.context.parser.ContextParser;
 import org.fundacionjala.virtualassistant.context.repository.ContextRepository;
 import org.fundacionjala.virtualassistant.context.models.ContextEntity;
+import org.fundacionjala.virtualassistant.models.UserEntity;
+import org.fundacionjala.virtualassistant.user.repository.UserRepo;
 import org.fundacionjala.virtualassistant.util.either.ProcessorEither;
 import org.springframework.stereotype.Service;
 
@@ -22,24 +26,52 @@ public class ContextService {
 
     private ContextRepository contextRepository;
     private ProcessorEither<Exception, ContextResponse> processorEither;
+    private UserRepo userRepo;
 
     public List<ContextResponse> findContextByUserId(Long idUser) throws ContextException {
+        if(validationIfUserExist(idUser)){
+            throw new ContextNotFoundException(ContextRequestException.MESSAGE_INVALID_TITLE);
+        }
+
         List<ContextEntity> contextEntities = contextRepository.findByUserEntityIdUser(idUser);
         return convertListContextToResponse(contextEntities);
     }
 
     public ContextResponse saveContext(ContextRequest request) throws ContextException {
-        if (isNull(request)) {
-            throw new ContextException(ContextException.MESSAGE_CONTEXT_REQUEST_NULL);
-        }
-
+        verifyContextRequest(request);
         ContextEntity contextEntity = ContextParser.parseFrom(request);
         return ContextParser.parseFrom(contextRepository.save(contextEntity));
     }
 
-    public Optional<ContextResponse> findById(Long idContext) {
+    public Optional<ContextResponse> findById(Long idContext) throws ContextRequestException {
+        if(contextRepository.findById(idContext).isEmpty()){
+            throw new ContextRequestException(ContextRequestException.MESSAGE_INVALID_ID);
+        }
+
         Optional<ContextEntity> optionalContext = contextRepository.findById(idContext);
         return optionalContext.map(ContextParser::parseFrom);
+    }
+
+    public ContextResponse editContext(Long idContext, ContextRequest request) throws ContextException {
+        verifyContextRequest(request);
+        if (contextRepository.findById(idContext).isEmpty()) {
+            throw new ContextRequestException(ContextRequestException.MESSAGE_INVALID_ID);
+        }
+
+        ContextEntity contextEntity = ContextParser.parseFrom(idContext,request);
+        return ContextResponse.fromEntity(contextRepository.save(contextEntity));
+    };
+
+    public boolean deleteContext(Long idContext) throws ContextException {
+        try {
+             ContextEntity context = contextRepository.findById(idContext)
+                    .orElseThrow(() -> new ContextRequestException(ContextRequestException.MESSAGE_INVALID_ID));
+
+            contextRepository.delete(context);
+            return true;
+        } catch (ContextRequestException e) {
+            throw new ContextRequestException(ContextException.MESSAGE_DELETE_ERROR,e);
+        }
     }
 
     private List<ContextResponse> convertListContextToResponse(List<ContextEntity> entityList)
@@ -49,5 +81,20 @@ public class ContextService {
         }
 
         return getContextResponses(entityList);
+    }
+
+    private boolean validationIfUserExist(Long IdUser){
+        Optional<UserEntity> user = userRepo.findByIdUser(IdUser);
+        return user.isEmpty();
+    }
+
+    private void verifyContextRequest(ContextRequest request) throws ContextException {
+        if (isNull(request)) {
+            throw new ContextException(ContextException.MESSAGE_CONTEXT_REQUEST_NULL);
+        }
+
+        if (validationIfUserExist(request.getUserRequest().getIdUser())){
+            throw new ContextRequestException(ContextRequestException.MESSAGE_CONTEXT_ID_USER_DONT_EXIST);
+        }
     }
 }
