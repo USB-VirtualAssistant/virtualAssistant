3f71c807a6e2c73ee412adcaf278fbf9da5ff214
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-09-08 02:26:34 -0400

    VA-060[SerranoAlejandroSelaya]: add more exception cases for context

--- src/main/java/org/fundacionjala/virtualassistant/context/exception/ContextException.java
+++ src/main/java/org/fundacionjala/virtualassistant/context/exception/ContextException.java
@@ -1,9 +1,13 @@
 package org.fundacionjala.virtualassistant.context.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class ContextException extends Exception{
 
     public static final String MESSAGE_CONTEXT_NULL = "The object Context is null";
     public static final String MESSAGE_CONTEXT_REQUEST_NULL = "The Context request is null";
+    public static final String MESSAGE_DELETE_ERROR = "There are an error when delete context";
 
     public ContextException(String message) {
         super(message);
