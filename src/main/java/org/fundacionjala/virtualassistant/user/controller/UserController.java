c4c360961551430425b46d534b2ba975340f44cb
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-09-09 12:03:21 -0400

    VA-067[agonzalesb107]: Created put method to update spotify token on db, change value for data on UserEntity

--- src/main/java/org/fundacionjala/virtualassistant/user/controller/UserController.java
+++ src/main/java/org/fundacionjala/virtualassistant/user/controller/UserController.java
@@ -3,7 +3,9 @@ package org.fundacionjala.virtualassistant.user.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.fundacionjala.virtualassistant.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.fundacionjala.virtualassistant.user.controller.request.UserRequest;
@@ -11,17 +13,23 @@ import org.fundacionjala.virtualassistant.user.controller.response.UserResponse;
 import org.fundacionjala.virtualassistant.user.exception.UserRequestException;
 import org.springframework.web.bind.annotation.RequestBody;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/")
 public class UserController {
     @Autowired
     UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) throws UserRequestException{
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.save(userRequest);
         return new ResponseEntity<>(userResponse, CREATED);
     }
 
+    @PutMapping("updateUser/updateSpotifyToken/{id}")
+    public ResponseEntity<UserResponse> updateSpotifyToken(@PathVariable Long id, @RequestBody UserRequest userRequest) throws UserRequestException {
+        UserResponse userResponse = userService.updateSpotifyToken(id, userRequest);
+        return new ResponseEntity<>(userResponse, OK);
+    }
 }
