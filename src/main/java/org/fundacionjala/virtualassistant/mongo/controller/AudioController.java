0328d194592bd60163016a4e9c584cf0e6223e88
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-09-12 14:37:22 -0400

    VA-074 [@santihsdev]: Add controller to process audio (save it in redis and process depending on its task)

---
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/AudioController.java
@@ -0,0 +1,33 @@
+package org.fundacionjala.virtualassistant.mongo.controller;
+
+import lombok.AllArgsConstructor;
+import org.fundacionjala.virtualassistant.asrOpenAiIntegration.service.AsrOpenAiImplementation;
+import org.fundacionjala.virtualassistant.mongo.controller.request.RecordingRequest;
+import org.fundacionjala.virtualassistant.mongo.services.RecordingService;
+import org.fundacionjala.virtualassistant.redis.service.AudioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/audio")
+@AllArgsConstructor
+public class AudioController {
+    private RecordingService recordingService;
+    private AsrOpenAiImplementation asrOpenAiImplementation;
+    private AudioService audioService;
+
+    @PostMapping
+    public ResponseEntity<?> processAudio(@Valid @ModelAttribute RecordingRequest recordingRequest) {
+        try {
+            audioService.save(recordingRequest.getAudioFile());
+            return ResponseEntity.ok(asrOpenAiImplementation.asrOpenAIResponse(recordingRequest.getAudioFile()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
