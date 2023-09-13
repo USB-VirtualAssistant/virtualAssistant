d903c1cf8cace9abb821d2a9d02532ed965e6898
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-09-12 20:35:53 -0400

    [VA-075-fix-taskhandler:@SebaRosales]Improvement over TaskAction

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/TaskAction.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/TaskAction.java
@@ -3,5 +3,5 @@ package org.fundacionjala.virtualassistant.taskhandler;
 import org.fundacionjala.virtualassistant.taskhandler.intents.EntityArgs;
 
 public interface TaskAction {
-    String handleAction(EntityArgs intentEntities);
+    String handleAction(EntityArgs intentEntities, String text);
 }
