3fd452143b0875d4fbcfc6bfd2e202ccd20609fd
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-09-12 20:32:19 -0400

    [VA-075-fix-taskhandler:@SebaRosales]Improved Action over response

--- src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/ContinueAction.java
+++ src/main/java/org/fundacionjala/virtualassistant/taskhandler/actions/ContinueAction.java
@@ -14,7 +14,7 @@ public class ContinueAction implements TaskAction {
     }
 
     @Override
-    public String handleAction(EntityArgs intentEntities) {
+    public String handleAction(EntityArgs intentEntities, String text) {
         return musicService.playCurrentTrack().getBody();
     }
 }
