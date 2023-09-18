6344533e50491eb31c26e564aa2099e721f1907b
Santi Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-09-18 12:39:23 -0400

    VA-084 [@santihsdev]: Save the text processed from audio and also save this text processed from the proxy as a response

--- src/main/java/org/fundacionjala/virtualassistant/mongo/controller/AudioProcessingController.java
+++ src/main/java/org/fundacionjala/virtualassistant/mongo/controller/AudioProcessingController.java
@@ -3,8 +3,14 @@ package org.fundacionjala.virtualassistant.mongo.controller;
 import lombok.AllArgsConstructor;
 import org.fundacionjala.virtualassistant.asrOpenAiIntegration.service.AsrOpenAiImplementation;
 import org.fundacionjala.virtualassistant.mongo.controller.request.RecordingRequest;
+import org.fundacionjala.virtualassistant.mongo.controller.response.RecordingResponse;
+import org.fundacionjala.virtualassistant.mongo.exception.RecordingException;
 import org.fundacionjala.virtualassistant.mongo.services.RecordingService;
+import org.fundacionjala.virtualassistant.parser.exception.ParserException;
+import org.fundacionjala.virtualassistant.redis.exception.FileSaveException;
 import org.fundacionjala.virtualassistant.redis.service.AudioService;
+import org.fundacionjala.virtualassistant.taskhandler.exception.IntentException;
+import org.fundacionjala.virtualassistant.textrequest.exception.TextRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/audio")
@@ -22,13 +29,12 @@ public class AudioProcessingController {
     private AudioService audioService;
 
     @PostMapping
-    public ResponseEntity<?> processAudio(@Valid @ModelAttribute RecordingRequest recordingRequest) {
-        try {
-            audioService.save(recordingRequest.getAudioFile());
-            recordingService.saveRecording(recordingRequest);
-            return ResponseEntity.ok(asrOpenAiImplementation.asrOpenAIResponse(recordingRequest.getAudioFile()));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<?> processAudio(@Valid @ModelAttribute RecordingRequest recordingRequest)
+            throws FileSaveException, RecordingException, ParserException,
+            IntentException, TextRequestException, IOException {
+        audioService.save(recordingRequest.getAudioFile());
+        RecordingResponse recordingResponse = recordingService.saveRecording(recordingRequest);
+        return ResponseEntity.ok(asrOpenAiImplementation
+                .asrOpenAIResponse(recordingRequest, recordingResponse.getIdRecording()));
     }
 }
